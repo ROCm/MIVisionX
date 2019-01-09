add8c1da92f3bb07a779eb553dd80772540aced2
Kiriti Gowda <kiritigowda@gmail.com>
2019-01-09 12:10:45 -0800

    Model Compiler Enhancements (#11)
    * Name changes to model compiler scripts
    
    * ONNX 1.3 Support
    
    * Removed Output Tensor print on Screen
    
    * model compiler readme updates

--- model_compiler/python/nnir_to_openvx.py
+++ model_compiler/python/nnir_to_openvx.py
@@ -1662,8 +1662,6 @@ Usage: python nnir2openvx.py [OPTIONS] <nnirInputFolder> <outputFolder>
     print('reading IR model from ' + inputFolder + ' ...')
     graph = IrGraph()
     graph.fromFile(inputFolder)
-    for tensor in graph.outputs:
-        print('#OUTPUT-TENSOR: %s %d %d %d %d ' %(tensor.name, tensor.shape[0], tensor.shape[1], tensor.shape[2], tensor.shape[3]));
     print('creating C code in ' + outputFolder + ' ...')
     generateCode(graph,argmaxOutput,outputFolder)
 

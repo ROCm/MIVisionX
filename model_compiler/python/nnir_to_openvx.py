5cb9f89176b83cea0f297dc233ab743f8026898e
LakshmiKumar23 <kumar.lakshmi1994@gmail.com>
2019-07-02 13:35:31 -0700

    Update nnir_to_openvx.py

--- model_compiler/python/nnir_to_openvx.py
+++ model_compiler/python/nnir_to_openvx.py
@@ -629,9 +629,7 @@ VX_API_ENTRY vx_status VX_API_CALL annAddToGraph(vx_graph graph, %s, %s, const c
             elif node.type == 'prior_box':
                 aspect_ratio = node.attr.get('aspect_ratio')
                 aspect_ratio_len = len(aspect_ratio)
-                aspect_ratio_str = ','.join(str(e) for e in aspect_ratio)
                 variance = node.attr.get('variance')
-                variance_str = ','.join(str(e) for e in variance)
                 max_size = node.attr.get('max_size')
                 f.write( \
 """

b6cd08aa9548999336d65bcccd6cdf0b1a12e826
reza <Seyedreza.Najafi@amd.com>
2019-10-08 18:13:19 -0700

    Corrected invalid test case

--- apps/augmentation_unittest/augmentation_unittests.cpp
+++ apps/augmentation_unittest/augmentation_unittests.cpp
@@ -148,7 +148,7 @@ int test(int test_case, const char* path, int rgb, int gpu, int display  )
             break;
         case 2: {
             std::cout << ">>>>>>> Running " << "raliCropResizeFixed" << std::endl;
-            image1 = raliCropResizeFixed(handle, image0, resize_w, resize_h, true, NULL, NULL, NULL);
+            image1 = raliCropResizeFixed(handle, image0, resize_w, resize_h, true, 0.8, 0.6, -0.4);
         }
             break;
         case 3: {

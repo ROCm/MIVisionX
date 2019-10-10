b23758fcee322ab5d15b2d8dad4a00a84ed4b188
reza <Seyedreza.Najafi@amd.com>
2019-10-09 17:28:28 -0700

    bug fix causes wrong remaining count

--- rali/source/master_graph.cpp
+++ rali/source/master_graph.cpp
@@ -94,14 +94,12 @@ MasterGraph::run()
             loader_image->pop_name();
     }
 
+    _ring_buffer.get_read_buffers();// make sure read buffers are ready, it'll wait here otherwise
     {
         std::unique_lock<std::mutex> lock(_count_lock);
         if (_in_process_count > 0)
             _in_process_count--;
     }
-
-    _ring_buffer.get_read_buffers();// make sure read buffers are ready, it'll wait here otherwise
-
     return MasterGraph::Status::OK;
 }
 
@@ -298,6 +296,7 @@ MasterGraph::internal_image_count()
 size_t
 MasterGraph::remaining_images_count()
 {
+
     std::unique_lock<std::mutex> lock(_count_lock);
     size_t ret = internal_image_count() + _in_process_count;
     return ret;

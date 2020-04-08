de4f22413b6561d71e9bc7b72ab0107f979f9861
LakshmiKumar23 <kumar.lakshmi1994@gmail.com>
2020-04-08 13:06:50 -0700

    Updating MIVIsionX version

--- MIVisionX-setup.py
+++ MIVisionX-setup.py
@@ -1,7 +1,7 @@
 __author__      = "Kiriti Nagesh Gowda"
 __copyright__   = "Copyright 2018, AMD Radeon MIVisionX setup"
 __license__     = "MIT"
-__version__     = "1.7"
+__version__     = "1.7.1"
 __maintainer__  = "Kiriti Nagesh Gowda"
 __email__       = "Kiriti.NageshGowda@amd.com"
 __status__      = "Shipping"

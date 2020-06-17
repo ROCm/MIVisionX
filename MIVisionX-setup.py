52e8535f038f1ece8766ac20c0cac0fc588a77a3
Kiriti <kiritigowda@gmail.com>
2020-06-17 13:28:29 -0700

    Setup Script Version & License

--- MIVisionX-setup.py
+++ MIVisionX-setup.py
@@ -1,7 +1,27 @@
+# Copyright (c) 2018 - 2020 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 __author__      = "Kiriti Nagesh Gowda"
-__copyright__   = "Copyright 2018, AMD Radeon MIVisionX setup"
+__copyright__   = "Copyright 2018 - 2020, AMD Radeon MIVisionX setup"
 __license__     = "MIT"
-__version__     = "1.7.9"
+__version__     = "1.7.10"
 __maintainer__  = "Kiriti Nagesh Gowda"
 __email__       = "Kiriti.NageshGowda@amd.com"
 __status__      = "Shipping"
@@ -290,4 +310,4 @@ else:
 			os.system('(cd '+deps_dir+'/ffmpeg; make -j8 )')
 			os.system('sudo -v')
 			os.system('(cd '+deps_dir+'/ffmpeg; sudo '+linuxFlag+' make install )')
-	print("\nMIVisionX Dependencies Installed\n")
+	print("\nMIVisionX Dependencies Installed with MIVisionX-setup.py V-"+__version__+"\n")

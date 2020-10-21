cda2e32ec5ebb64a5d5a7f09b0008b169d885cd4
fiona-gladwin <fionagladwin@multicorewareinc.com>
2020-10-21 18:49:34 +0530

    Minor Changes to Test suite

--- samples/hip_samples/hip-openvx/main.cpp
+++ samples/hip_samples/hip-openvx/main.cpp
@@ -14,7 +14,7 @@
 #include <string>
 #include <chrono>
 #define DUMP_IMAGE  0
-#define PRINT_OUTPUT 
+#define PRINT_OUTPUT 1
 
 #define ERROR_CHECK_OBJECT(obj) { vx_status status = vxGetStatus((vx_reference)(obj)); if(status != VX_SUCCESS) { vxAddLogEntry((vx_reference)context, status     , "ERROR: failed with status = (%d) at " __FILE__ "#%d\n", status, __LINE__); return status; } }
 #define ERROR_CHECK_STATUS(call) { vx_status status = (call); if(status != VX_SUCCESS) { printf("ERROR: failed with status = (%d) at " __FILE__ "#%d\n", status, __LINE__); return -1; } }
@@ -46,7 +46,6 @@ vx_status makeInputImage1(vx_context context, vx_image img, int width, int heigh
 	ERROR_CHECK_OBJECT((vx_reference)img);
 
 	if (mem_type == VX_MEMORY_TYPE_HOST) {
-		// vx_int16 image_data[width*height];
 		vx_uint8 image_data[width*height];
 		for (int i= 0; i< height; i++ ){
 			for (int j=0; j < width; j++) {
@@ -92,7 +91,7 @@ vx_status makeInputImage2(vx_context context, vx_image img, vx_uint32 width, vx_
 		vx_uint8 image_data[width*height];
 		for (int i= 0; i< height; i++ ){
 			for (int j=0; j < width; j++) {
-				if ( i>=0.4*width && i < 0.6*width && j >= 0.2*height && j<0.8*width )
+				if ( i>=0.4*height && i < 0.6*height && j >= 0.2*width && j<0.8*width )
 					image_data[i*width + j] = inner_pix_val;
 				else
 					image_data[i*width + j] = outer_pix_val;
@@ -113,7 +112,7 @@ vx_status makeInputImage2(vx_context context, vx_image img, vx_uint32 width, vx_
 		ERROR_CHECK_STATUS(vxMapImagePatch(img, &rect, 0, &map_id, &addrId, (void **)&ptr, VX_WRITE_ONLY, VX_MEMORY_TYPE_HOST, VX_NOGAP_X));
 		for (int i= 0; i< height; i++ ){
 			for (int j=0; j < width; j++) {
-				if ( i>=0.4*width && i < 0.6*width && j >= 0.2*height && j<0.8*width )
+				if ( i>=0.4*height && i < 0.6*height && j >= 0.2*width && j<0.8*width )
 					ptr[i*width + j] = inner_pix_val;
 				else
 					ptr[i*width + j] = outer_pix_val;
@@ -135,18 +134,12 @@ vx_status makeInputImage2(vx_context context, vx_image img, vx_uint32 width, vx_
 int main(int argc, const char ** argv) {
 
 	 // check command-line usage
-    const size_t MIN_ARG_COUNT = 7;
+    const size_t MIN_ARG_COUNT = 4;
     if(argc < MIN_ARG_COUNT){
    	 printf( "Usage: ./hipvx_sample <width> <height> <gpu=1/cpu=0> <image1 pixel value> <image2 outer pixel value> <image2 inner pixel value>\n" );
         return -1;
     }
-	//image pixel values  user given
-	int pix_img1, pix_outer_img2, pix_inner_img2;
-	pix_img1 =  atoi(argv[4]);
-	pix_outer_img2 =  atoi(argv[5]);
-	pix_inner_img2 =  atoi(argv[6]);
-
-
+	
 	void *ptr[3] = {nullptr, nullptr, nullptr};     // hip
 	int64_t freq = clockFrequency(), t0, t1;
 	vx_image img1, img2, img_out;
@@ -158,6 +151,14 @@ int main(int argc, const char ** argv) {
 	if (height <= 0) height = 100;
 	if (affinity <= 0) affinity = 0;
 	char* outImgBuffer = new char[width*height];
+
+	//image pixel values  user given
+	int pix_img1, pix_outer_img2, pix_inner_img2;
+	pix_img1 = (argc < 5) ?  100 : atoi(argv[4]);
+	pix_outer_img2 =  (argc < 6) ?  100 : atoi(argv[5]);
+	pix_inner_img2 =  (argc < 7) ?  100 : atoi(argv[6]);
+
+
 
 	// create context, input, output, and graph
 	vxRegisterLogCallback(NULL, log_callback, vx_false_e);
@@ -194,10 +195,12 @@ int main(int argc, const char ** argv) {
 			// vx_node node = vxAbsDiffNode(graph, img1, img2, img_out);
 			// vx_node node = vxAddNode(graph, img1, img2, VX_CONVERT_POLICY_WRAP, img_out);
 			// vx_node node = vxAddNode(graph, img1, img2, VX_CONVERT_POLICY_SATURATE, img_out);
-			// vx_node node = vxSubtractNode(graph, img1, img2, VX_CONVERT_POLICY_WRAP, img_out);
-			vx_node node = vxSubtractNode(graph, img1, img2, VX_CONVERT_POLICY_SATURATE, img_out);
+			vx_node node = vxSubtractNode(graph, img1, img2, VX_CONVERT_POLICY_WRAP, img_out);
+			// vx_node node = vxSubtractNode(graph, img1, img2, VX_CONVERT_POLICY_SATURATE, img_out);
 			// vx_node node = vxMultiplyNode(graph, img1, img2, VX_CONVERT_POLICY_WRAP, VX_ROUND_POLICY_TO_ZERO, img_out);
-		
+			// vx_node node = vxOrNode(graph, img1, img2, img_out);
+			// vx_node node = vxXorNode(graph, img1, img2, img_out);
+
 			if (node)
 			{
 				status = vxVerifyGraph(graph);
@@ -251,13 +254,12 @@ int main(int argc, const char ** argv) {
 			// vx_node node = vxAbsDiffNode(graph, img1, img2, img_out);
 			// vx_node node = vxAddNode(graph, img1, img2, VX_CONVERT_POLICY_WRAP, img_out);
 			// vx_node node = vxAddNode(graph, img1, img2, VX_CONVERT_POLICY_SATURATE, img_out);
-			// vx_node node = vxSubtractNode(graph, img1, img2, VX_CONVERT_POLICY_WRAP, img_out);
-			vx_node node = vxSubtractNode(graph, img1, img2, VX_CONVERT_POLICY_SATURATE, img_out);
+			vx_node node = vxSubtractNode(graph, img1, img2, VX_CONVERT_POLICY_WRAP, img_out);
+			// vx_node node = vxSubtractNode(graph, img1, img2, VX_CONVERT_POLICY_SATURATE, img_out);
+			// vx_node node = vxOrNode(graph, img1, img2, img_out);
+			// vx_node node = vxXorNode(graph, img1, img2, img_out);
 
 			
-
-
-
 
 			if (node)
 			{
@@ -297,15 +299,20 @@ int main(int argc, const char ** argv) {
 
 
 	// VERIFYING KERNELS
-	
+	int h_rect = (int)(0.6*height) - (int)(0.4*height);
+	int w_rect = (int)(0.8*width) - (int)(0.2*width);
 	// int expected = 20*60*255;     // white only in roi 
 	// int expected = (115 * 20 * 60); // AbsDiff
-	// int expected = (120 * 100 * 100) + (5 * 20 * 60) +  (120 * ((100 * 100) - (20 * 60))); // Add
-	// int expected = (140 * 100 * 100) + (5 * 20 * 60) +  (140 * ((100 * 100) - (20 * 60))); // Add
-	// int expected = 20 * 60 * 5; //ADD_S16_S16U8_Wrap
-	int expected =  (width*height * pix_img1) -(width*height*pix_outer_img2 - (0.2*width *0.6*height *pix_outer_img2) + (0.2*width *0.6*height *pix_inner_img2)); //SUB_U8_U8U8_Wrap & SUB_U8_U8U8_Sat
+
+	// int expected =  ((h_rect*w_rect) * (pix_img1 + pix_inner_img2)) + (((width * height) - (h_rect*w_rect)) * (pix_img1 + pix_outer_img2)); //Add_U8_U8U8_Wrap & Add_U8_U8U8_Sat
+	int expected =  ((h_rect*w_rect) * (pix_img1 - pix_inner_img2)) + (((width * height) - (h_rect*w_rect)) * (pix_img1 - pix_outer_img2)); //Sub_U8_U8U8_Wrap & Sub_U8_U8U8_Sat
+
+	// int expected =  ((h_rect*w_rect) * (pix_img1 | pix_inner_img2)) + (((width * height) - (h_rect*w_rect)) * (pix_img1 | pix_outer_img2)); // Or_U8_U8U8
+	// int expected =  ((h_rect*w_rect) * (pix_img1 ^ pix_inner_img2)) + (((width * height) - (h_rect*w_rect)) * (pix_img1 ^ pix_outer_img2)); // Xor_U8_U8U8
+	
+	
 	//PRINT OUTPUT IMAGE
-#ifdef PRINT_OUTPUT
+#if PRINT_OUTPUT
 	 int i,j;
        for ( i = 0; i< height ; i++, printf("\n"))
        {
@@ -335,7 +342,7 @@ int main(int argc, const char ** argv) {
 	// free the resources on host side
 	if (ptr[0]) hipFree(ptr[0]);
 	if (ptr[1]) hipFree(ptr[1]);
-	if (ptr[1]) hipFree(ptr[1]);
+	if (ptr[2]) hipFree(ptr[2]);
 	free(outImgBuffer);
 	vxReleaseContext(&context);
 

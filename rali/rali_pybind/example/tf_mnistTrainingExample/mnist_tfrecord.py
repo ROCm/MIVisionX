5ba4c5a84ca7039cc370580b4dd3262ad59af68d
rrawther <Rajy.MeeyakhanRawther@amd.com>
2020-10-21 10:37:25 -0700

    removed some codecy warnings

--- rali/rali_pybind/example/tf_mnistTrainingExample/mnist_tfrecord.py
+++ rali/rali_pybind/example/tf_mnistTrainingExample/mnist_tfrecord.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 def _data_path(data_directory:str, name:str) -> str:
-    """Construct a full path to a TFRecord file to be stored in the 
+    """Construct a full path to a TFRecord file to be stored in the
     data_directory. Will also ensure the data directory exists
     
     Args:
--- rali/rali_pybind/example/tf_mnistTrainingExample/tf_mnist_classification_rali.py
+++ rali/rali_pybind/example/tf_mnistTrainingExample/tf_mnist_classification_rali.py
@@ -184,4 +184,4 @@ def main():
 	train_mnist_rali(image_path, _rali_cpu, bs)
 
 if __name__ == '__main__':
-    main() 
+	main()

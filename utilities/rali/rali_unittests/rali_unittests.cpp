c60315250898bd9a13e2127548e06465f9c55309
IndumathiR <indumathi@multicorewareinc.com>
2020-11-02 10:37:16 +0530

    Introduce data loader for coco reader using partail decoder

--- utilities/rali/rali_unittests/rali_unittests.cpp
+++ utilities/rali/rali_unittests/rali_unittests.cpp
@@ -35,8 +35,9 @@ THE SOFTWARE.
 
 using namespace cv;
 
- #define PARTIAL_DECODE
-// #define COCO_READER
+ //#define PARTIAL_DECODE
+ #define COCO_READER
+//#define COCO_READER_PARTIAL
 // #define TF_READER
 // #define TF_READER_DETECTION
 // #define CAFFE2_READER
@@ -139,7 +140,7 @@ int test(int test_case, const char *path, const char *outName, int rgb, int gpu,
     char key7[25] = "image/object/bbox/ymax";
     char key8[25] = "image/filename";
 
-#ifdef COCO_READER
+#if defined COCO_READER || defined COCO_READER_PARTIAL
     char *json_path = "/home/shobana/sample_test/coco_10_samples/10_instances_train2017.json";
     if (strcmp(json_path, "") == 0)
     {
@@ -192,6 +193,8 @@ int test(int test_case, const char *path, const char *outName, int rgb, int gpu,
     else
         input1 = raliJpegCOCOFileSource(handle, path, json_path, color_format, num_threads, false, true, false,
                                         RALI_USE_USER_GIVEN_SIZE, decode_max_width, decode_max_height);
+#elif defined COCO_READER_PARTIAL
+        input1 = raliJpegCOCOFileSourcePartial(handle, path, json_path, color_format, num_threads, false, true, false);
 #else
     if (decode_max_height <= 0 || decode_max_width <= 0)
         input1 = raliJpegFileSource(handle, path, color_format, num_threads, false, true);
@@ -629,7 +632,7 @@ int test(int test_case, const char *path, const char *outName, int rgb, int gpu,
             break;
         int label_id[inputBatchSize];
         int image_name_length[inputBatchSize];
-#if defined COCO_READER || defined CAFFE_READER_DETECTION || defined CAFFE2_READER_DETECTION || defined TF_READER_DETECTION
+#if defined COCO_READER || defined COCO_READER_PARTIAL || defined CAFFE_READER_DETECTION || defined CAFFE2_READER_DETECTION || defined TF_READER_DETECTION
         int img_size = raliGetImageNameLen(handle, image_name_length);
         char img_name[img_size];
         raliGetImageName(handle, img_name);

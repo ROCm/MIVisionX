1ae8ba0548b842032f25a447cfc25910fbf9e834
LakshmiKumar23 <kumar.lakshmi1994@gmail.com>
2020-12-07 01:35:32 -0800

    addign support for u1 Or nodes

--- samples/hip_samples/hipvx_unittests/main.cpp
+++ samples/hip_samples/hipvx_unittests/main.cpp
@@ -1136,7 +1136,7 @@ int main(int argc, const char ** argv)
 				{
 					// test_case_name = "agoKernel_And_U8_U8U1";
 					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
-					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
+					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1_AMD);
 					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
 					ERROR_CHECK_STATUS(vxGetStatus((vx_reference)img_out));
 					node = vxAndNode(graph, img1, img2, img_out);
@@ -1276,7 +1276,7 @@ int main(int argc, const char ** argv)
 				{
 					// test_case_name = "agoKernel_Or_U8_U8U1";
 					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
-					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
+					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
 					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
 					ERROR_CHECK_STATUS(vxGetStatus((vx_reference)img_out));
 					node = vxOrNode(graph, img1, img2, img_out);
@@ -1287,7 +1287,7 @@ int main(int argc, const char ** argv)
 				case 51:
 				{
 					// test_case_name = "agoKernel_Or_U8_U1U8";
-					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
+					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
 					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
 					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
 					ERROR_CHECK_STATUS(vxGetStatus((vx_reference)img_out));
@@ -1299,8 +1299,8 @@ int main(int argc, const char ** argv)
 				case 52:
 				{
 					// test_case_name = "agoKernel_Or_U8_U1U1";
-					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
-					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
+					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
+					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
 					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
 					ERROR_CHECK_STATUS(vxGetStatus((vx_reference)img_out));
 					node = vxOrNode(graph, img1, img2, img_out);
@@ -1324,8 +1324,8 @@ int main(int argc, const char ** argv)
 				{
 					// test_case_name = "agoKernel_Or_U1_U8U1";
 					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
-					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
-					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
+					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
+					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U1_AMD);
 					ERROR_CHECK_STATUS(vxGetStatus((vx_reference)img_out));
 					node = vxOrNode(graph, img1, img2, img_out);
 					expected_image_sum = (pix_img1_u8 | pix_img2_u1) * width * height;
@@ -1335,9 +1335,9 @@ int main(int argc, const char ** argv)
 				case 55:
 				{
 					// test_case_name = "agoKernel_Or_U1_U1U8";
-					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
+					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
 					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
-					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
+					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U1_AMD);
 					ERROR_CHECK_STATUS(vxGetStatus((vx_reference)img_out));
 					node = vxOrNode(graph, img1, img2, img_out);
 					expected_image_sum = (pix_img1_u1 | pix_img2_u8) * width * height;
@@ -1347,9 +1347,9 @@ int main(int argc, const char ** argv)
 				case 56:
 				{
 					// test_case_name = "agoKernel_Or_U1_U1U1";
-					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
-					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
-					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U8);
+					img1 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
+					img2 = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
+					img_out = vxCreateImage(context, width, height, VX_DF_IMAGE_U1);
 					ERROR_CHECK_STATUS(vxGetStatus((vx_reference)img_out));
 					node = vxOrNode(graph, img1, img2, img_out);
 					expected_image_sum = (pix_img1_u1 | pix_img2_u1) * width * height;

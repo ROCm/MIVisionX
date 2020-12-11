ba1a94c671bbff5e01928802a8d5609e501f33b9
LakshmiKumar23 <kumar.lakshmi1994@gmail.com>
2020-12-10 23:47:04 -0800

    indentation correction

--- model_compiler/python/nnir_to_openvx.py
+++ model_compiler/python/nnir_to_openvx.py
@@ -1648,7 +1648,7 @@ VX_API_ENTRY int VX_API_CALL annCopyFromInferenceLocal(pyif_ann_handle handle, c
         printf("ERROR: annCopyFromInferenceLocal: invalid output buffer size (must be %%d) -- got %%d\\n", (int)stride[3],(int)out_size);
     }
 """ % (input_shape[0]))
-                elif input_data_type == "F016":
+            	elif input_data_type == "F016":
 	                f.write (\
 """     else if(out_size/(2*%d) != stride[3]) {
         status = VX_FAILURE;

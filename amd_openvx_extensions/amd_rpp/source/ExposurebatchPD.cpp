8e1b0ded9a4c69d3cd7c1739a04a3f4a2255687b
shobana-mcw <shobana@multicorewareinc.com>
2020-12-27 20:56:43 +0530

    Add RPP HIP call support for ExposurebatchPD and FisheyebatchPD.

--- amd_openvx_extensions/amd_rpp/source/ExposurebatchPD.cpp
+++ amd_openvx_extensions/amd_rpp/source/ExposurebatchPD.cpp
@@ -35,6 +35,9 @@ struct ExposurebatchPDLocalData {
 #if ENABLE_OPENCL
 	cl_mem cl_pSrc;
 	cl_mem cl_pDst;
+#elif ENABLE_HIP
+	void *hip_pSrc;
+	void *hip_pDst;
 #endif 
 };
 
@@ -63,6 +66,9 @@ static vx_status VX_CALLBACK refreshExposurebatchPD(vx_node node, const vx_refer
 #if ENABLE_OPENCL
 		STATUS_ERROR_CHECK(vxQueryImage((vx_image)parameters[0], VX_IMAGE_ATTRIBUTE_AMD_OPENCL_BUFFER, &data->cl_pSrc, sizeof(data->cl_pSrc)));
 		STATUS_ERROR_CHECK(vxQueryImage((vx_image)parameters[3], VX_IMAGE_ATTRIBUTE_AMD_OPENCL_BUFFER, &data->cl_pDst, sizeof(data->cl_pDst)));
+#elif ENABLE_HIP
+		STATUS_ERROR_CHECK(vxQueryImage((vx_image)parameters[0], VX_IMAGE_ATTRIBUTE_AMD_HIP_BUFFER, &data->hip_pSrc, sizeof(data->hip_pSrc)));
+		STATUS_ERROR_CHECK(vxQueryImage((vx_image)parameters[3], VX_IMAGE_ATTRIBUTE_AMD_HIP_BUFFER, &data->hip_pDst, sizeof(data->hip_pDst)));
 #endif
 	}
 	if(data->device_type == AGO_TARGET_AFFINITY_CPU) {
@@ -129,6 +135,15 @@ static vx_status VX_CALLBACK processExposurebatchPD(vx_node node, const vx_refer
 			status = rppi_exposure_u8_pkd3_batchPD_gpu((void *)data->cl_pSrc,data->srcDimensions,data->maxSrcDimensions,(void *)data->cl_pDst,data->exposureValue,data->nbatchSize,data->rppHandle);
 		}
 		return status;
+#elif ENABLE_HIP
+		refreshExposurebatchPD(node, parameters, num, data);
+		if (df_image == VX_DF_IMAGE_U8 ){ 
+ 			status = rppi_exposure_u8_pln1_batchPD_gpu((void *)data->hip_pSrc,data->srcDimensions,data->maxSrcDimensions,(void *)data->hip_pDst,data->exposureValue,data->nbatchSize,data->rppHandle);
+		}
+		else if(df_image == VX_DF_IMAGE_RGB) {
+			status = rppi_exposure_u8_pkd3_batchPD_gpu((void *)data->hip_pSrc,data->srcDimensions,data->maxSrcDimensions,(void *)data->hip_pDst,data->exposureValue,data->nbatchSize,data->rppHandle);
+		}
+		return status;
 #endif
 	}
 	if(data->device_type == AGO_TARGET_AFFINITY_CPU) {
@@ -149,12 +164,17 @@ static vx_status VX_CALLBACK initializeExposurebatchPD(vx_node node, const vx_re
 	memset(data, 0, sizeof(*data));
 #if ENABLE_OPENCL
 	STATUS_ERROR_CHECK(vxQueryNode(node, VX_NODE_ATTRIBUTE_AMD_OPENCL_COMMAND_QUEUE, &data->handle.cmdq, sizeof(data->handle.cmdq)));
+#elif ENABLE_HIP
+	STATUS_ERROR_CHECK(vxQueryNode(node, VX_NODE_ATTRIBUTE_AMD_HIP_STREAM, &data->handle.hipstream, sizeof(data->handle.hipstream)));
 #endif
 	STATUS_ERROR_CHECK(vxCopyScalar((vx_scalar)parameters[6], &data->device_type, VX_READ_ONLY, VX_MEMORY_TYPE_HOST));
 	refreshExposurebatchPD(node, parameters, num, data);
 #if ENABLE_OPENCL
 	if(data->device_type == AGO_TARGET_AFFINITY_GPU)
 		rppCreateWithStreamAndBatchSize(&data->rppHandle, data->handle.cmdq, data->nbatchSize);
+#elif ENABLE_HIP
+	if(data->device_type == AGO_TARGET_AFFINITY_GPU)
+		rppCreateWithStreamAndBatchSize(&data->rppHandle, data->handle.hipstream, data->nbatchSize);
 #endif
 	if(data->device_type == AGO_TARGET_AFFINITY_CPU)
 		rppCreateWithBatchSize(&data->rppHandle, data->nbatchSize);
@@ -167,7 +187,7 @@ static vx_status VX_CALLBACK uninitializeExposurebatchPD(vx_node node, const vx_
 {
 	ExposurebatchPDLocalData * data; 
 	STATUS_ERROR_CHECK(vxQueryNode(node, VX_NODE_LOCAL_DATA_PTR, &data, sizeof(data)));
-#if ENABLE_OPENCL
+#if ENABLE_OPENCL || ENABLE_HIP
 	if(data->device_type == AGO_TARGET_AFFINITY_GPU)
 		rppDestroyGPU(data->rppHandle);
 #endif
@@ -191,7 +211,7 @@ vx_status ExposurebatchPD_Register(vx_context context)
 	ERROR_CHECK_OBJECT(kernel);
 	AgoTargetAffinityInfo affinity;
 	vxQueryContext(context, VX_CONTEXT_ATTRIBUTE_AMD_AFFINITY,&affinity, sizeof(affinity));
-#if ENABLE_OPENCL
+#if ENABLE_OPENCL || ENABLE_HIP
 	// enable OpenCL buffer access since the kernel_f callback uses OpenCL buffers instead of host accessible buffers
 	vx_bool enableBufferAccess = vx_true_e;
 	if(affinity.device_type == AGO_TARGET_AFFINITY_GPU)
--- amd_openvx_extensions/amd_rpp/source/FisheyebatchPD.cpp
+++ amd_openvx_extensions/amd_rpp/source/FisheyebatchPD.cpp
@@ -34,6 +34,9 @@ struct FisheyebatchPDLocalData {
 #if ENABLE_OPENCL
 	cl_mem cl_pSrc;
 	cl_mem cl_pDst;
+#elif ENABLE_HIP
+	void *hip_pSrc;
+	void *hip_pDst;
 #endif 
 };
 
@@ -59,6 +62,9 @@ static vx_status VX_CALLBACK refreshFisheyebatchPD(vx_node node, const vx_refere
 #if ENABLE_OPENCL
 		STATUS_ERROR_CHECK(vxQueryImage((vx_image)parameters[0], VX_IMAGE_ATTRIBUTE_AMD_OPENCL_BUFFER, &data->cl_pSrc, sizeof(data->cl_pSrc)));
 		STATUS_ERROR_CHECK(vxQueryImage((vx_image)parameters[3], VX_IMAGE_ATTRIBUTE_AMD_OPENCL_BUFFER, &data->cl_pDst, sizeof(data->cl_pDst)));
+#elif ENABLE_HIP
+		STATUS_ERROR_CHECK(vxQueryImage((vx_image)parameters[0], VX_IMAGE_ATTRIBUTE_AMD_HIP_BUFFER, &data->hip_pSrc, sizeof(data->hip_pSrc)));
+		STATUS_ERROR_CHECK(vxQueryImage((vx_image)parameters[3], VX_IMAGE_ATTRIBUTE_AMD_HIP_BUFFER, &data->hip_pDst, sizeof(data->hip_pDst)));
 #endif
 	}
 	if(data->device_type == AGO_TARGET_AFFINITY_CPU) {
@@ -125,6 +131,15 @@ static vx_status VX_CALLBACK processFisheyebatchPD(vx_node node, const vx_refere
 			status = rppi_fisheye_u8_pkd3_batchPD_gpu((void *)data->cl_pSrc,data->srcDimensions,data->maxSrcDimensions,(void *)data->cl_pDst,data->nbatchSize,data->rppHandle);
 		}
 		return status;
+#elif ENABLE_HIP
+		refreshFisheyebatchPD(node, parameters, num, data);
+		if (df_image == VX_DF_IMAGE_U8 ){ 
+ 			status = rppi_fisheye_u8_pln1_batchPD_gpu((void *)data->hip_pSrc,data->srcDimensions,data->maxSrcDimensions,(void *)data->hip_pDst,data->nbatchSize,data->rppHandle);
+		}
+		else if(df_image == VX_DF_IMAGE_RGB) {
+			status = rppi_fisheye_u8_pkd3_batchPD_gpu((void *)data->hip_pSrc,data->srcDimensions,data->maxSrcDimensions,(void *)data->hip_pDst,data->nbatchSize,data->rppHandle);
+		}
+		return status;
 #endif
 	}
 	if(data->device_type == AGO_TARGET_AFFINITY_CPU) {
@@ -145,12 +160,17 @@ static vx_status VX_CALLBACK initializeFisheyebatchPD(vx_node node, const vx_ref
 	memset(data, 0, sizeof(*data));
 #if ENABLE_OPENCL
 	STATUS_ERROR_CHECK(vxQueryNode(node, VX_NODE_ATTRIBUTE_AMD_OPENCL_COMMAND_QUEUE, &data->handle.cmdq, sizeof(data->handle.cmdq)));
+#elif ENABLE_HIP
+	STATUS_ERROR_CHECK(vxQueryNode(node, VX_NODE_ATTRIBUTE_AMD_HIP_STREAM, &data->handle.hipstream, sizeof(data->handle.hipstream)));
 #endif
 	STATUS_ERROR_CHECK(vxCopyScalar((vx_scalar)parameters[5], &data->device_type, VX_READ_ONLY, VX_MEMORY_TYPE_HOST));
 	refreshFisheyebatchPD(node, parameters, num, data);
 #if ENABLE_OPENCL
 	if(data->device_type == AGO_TARGET_AFFINITY_GPU)
 		rppCreateWithStreamAndBatchSize(&data->rppHandle, data->handle.cmdq, data->nbatchSize);
+#elif ENABLE_HIP
+	if(data->device_type == AGO_TARGET_AFFINITY_GPU)
+		rppCreateWithStreamAndBatchSize(&data->rppHandle, data->handle.hipstream, data->nbatchSize);
 #endif
 	if(data->device_type == AGO_TARGET_AFFINITY_CPU)
 		rppCreateWithBatchSize(&data->rppHandle, data->nbatchSize);
@@ -163,7 +183,7 @@ static vx_status VX_CALLBACK uninitializeFisheyebatchPD(vx_node node, const vx_r
 {
 	FisheyebatchPDLocalData * data; 
 	STATUS_ERROR_CHECK(vxQueryNode(node, VX_NODE_LOCAL_DATA_PTR, &data, sizeof(data)));
-#if ENABLE_OPENCL
+#if ENABLE_OPENCL || ENABLE_HIP
 	if(data->device_type == AGO_TARGET_AFFINITY_GPU)
 		rppDestroyGPU(data->rppHandle);
 #endif
@@ -187,7 +207,7 @@ vx_status FisheyebatchPD_Register(vx_context context)
 	ERROR_CHECK_OBJECT(kernel);
 	AgoTargetAffinityInfo affinity;
 	vxQueryContext(context, VX_CONTEXT_ATTRIBUTE_AMD_AFFINITY,&affinity, sizeof(affinity));
-#if ENABLE_OPENCL
+#if ENABLE_OPENCL || ENABLE_HIP
 	// enable OpenCL buffer access since the kernel_f callback uses OpenCL buffers instead of host accessible buffers
 	vx_bool enableBufferAccess = vx_true_e;
 	if(affinity.device_type == AGO_TARGET_AFFINITY_GPU)

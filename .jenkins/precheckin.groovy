9a41186b513082372b62dd0c9ac8ea8b8c150deb
Kiriti Nagesh Gowda <kiritigowda@gmail.com>
2021-02-03 23:43:52 -0800

    Jenkins - Fix Missing Hardware

--- .jenkins/precheckin.groovy
+++ .jenkins/precheckin.groovy
@@ -47,7 +47,7 @@ ci: {
     def propertyList = ["compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 1 * * 0')])]]
     propertyList = auxiliary.appendPropertyList(propertyList)
 
-    def jobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu18:['gfx906'], ubuntu20:['gfx908'], centos7:['gfx906'], centos8:['gfx908'], sles15sp1:['gfx906']])]
+    def jobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu18:['gfx908'], ubuntu20:['gfx906'], centos7:['gfx906'], centos8:['gfx908'], sles15sp1:['gfx906']])]
     jobNameList = auxiliary.appendJobNameList(jobNameList)
 
     propertyList.each 

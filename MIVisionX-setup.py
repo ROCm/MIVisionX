a5f6ee7ff5acf74285db9d20c791d4af9c5f7715
Kiriti Gowda <kiriti.nageshgowda@amd.com>
2021-05-19 08:18:15 -1000

    Docker - Dockerfiles update (#509)
    * Docker - Update boost 1.72 download
    
    * MIOpen - Updates

--- MIVisionX-setup.py
+++ MIVisionX-setup.py
@@ -44,8 +44,8 @@ parser.add_argument('--installer', 	type=str, default='apt-get',
                     help='Linux system installer - optional (default:apt-get) [options: Ubuntu - apt-get; CentOS - yum]')
 parser.add_argument('--opencv',    	type=str, default='3.4.0',
                     help='OpenCV Version - optional (default:3.4.0)')
-parser.add_argument('--miopen',    	type=str, default='2.10.0',
-                    help='MIOpen Version - optional (default:2.10.0)')
+parser.add_argument('--miopen',    	type=str, default='2.11.0',
+                    help='MIOpen Version - optional (default:2.11.0)')
 parser.add_argument('--miopengemm',	type=str, default='1.1.5',
                     help='MIOpenGEMM Version - optional (default:1.1.5)')
 parser.add_argument('--protobuf',  	type=str, default='3.12.0',

9f82e1c5e0711776582820249272237443a18949
Rajy Rawther <Rajy.MeeyakhanRawther@amd.com>
2021-07-29 15:45:17 -0700

    rocAL - coco meta_data_reader update for SSD (#579)
    * coco meta_reader optimization to remove reading metadata many times
    
    * fix build error
    
    * fix bug in checking reader type
    
    * adjust spacing

--- rocAL/rocAL/source/node_image_loader.cpp
+++ rocAL/rocAL/source/node_image_loader.cpp
@@ -35,7 +35,7 @@ ImageLoaderNode::ImageLoaderNode(Image *output, DeviceResources device_resources
 
 
 void ImageLoaderNode::init(unsigned internal_shard_count, const std::string &source_path, const std::string &json_path, const std::map<std::string, std::string> feature_key_map, StorageType storage_type,
-                           DecoderType decoder_type, bool shuffle, bool loop, size_t load_batch_count, RaliMemType mem_type, bool decoder_keep_orig, const char* file_prefix)
+                           DecoderType decoder_type, bool shuffle, bool loop, size_t load_batch_count, RaliMemType mem_type, std::shared_ptr<MetaDataReader> meta_data_reader, bool decoder_keep_orig, const char* file_prefix)
 {
     if(!_loader_module)
         THROW("ERROR: loader module is not set for ImageLoaderNode, cannot initialize")
@@ -47,6 +47,7 @@ void ImageLoaderNode::init(unsigned internal_shard_count, const std::string &sou
     reader_cfg.set_shard_count(internal_shard_count);
     reader_cfg.set_batch_count(load_batch_count);
     reader_cfg.set_file_prefix(file_prefix);
+    reader_cfg.set_meta_data_reader(meta_data_reader);
     _loader_module->initialize(reader_cfg, DecoderConfig(decoder_type),
              mem_type,
              _batch_size, decoder_keep_orig);

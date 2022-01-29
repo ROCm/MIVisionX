b4891c21ed3f4d418ac287879f7bf3038e179177
Swetha B S <swetha@multicorewareinc.com>
2022-01-29 16:29:26 +0000

    Minor Change in OpenCL spelling in the setup script

--- rocAL/rocAL_pybind/setup.py
+++ rocAL/rocAL_pybind/setup.py
@@ -38,7 +38,7 @@ class CMakeBuild(build_ext):
                       '-DPYTHON_EXECUTABLE=' + sys.executable, '-DGPU_SUPPORT=ON', '-DBACKEND=HIP']
         else:
             cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable, '-DGPU_SUPPORT=ON', '-DBACKEND=OCL']
+                      '-DPYTHON_EXECUTABLE=' + sys.executable, '-DGPU_SUPPORT=ON', '-DBACKEND=OPENCL']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]

8ba3f1d7060a232241b832ebc7fc83fe0bb9ab61
shobana-mcw <shobana@multicorewareinc.com>
2022-03-02 18:29:38 +0000

    rocAL code restructuring.

--- rocAL/rocAL/include/decoders/image/decoder.h
+++ rocAL/rocAL/include/decoders/image/decoder.h
@@ -32,7 +32,6 @@ enum class DecoderType
     FUSED_TURBO_JPEG = 1, //!< FOR PARTIAL DECODING
     OPENCV_DEC = 2, //!< for back_up decoding
     SKIP_DECODE  = 3, //!< For skipping decoding in case of uncompressed data from reader
-    OVX_FFMPEG,//!< Uses FFMPEG to decode video streams, can decode up to 4 video streams simultaneously
 };
 
 
@@ -74,26 +73,26 @@ public:
 
     enum class ColorFormat {
         GRAY = 0,
-        RGB, 
+        RGB,
         BGR
     };
     //! Decodes the header of the Jpeg compressed data and returns basic info about the compressed image
     /*!
      \param input_buffer  User provided buffer containig the encoded image
      \param input_size Size of the compressed data provided in the input_buffer
-     \param width pointer to the user's buffer to write the width of the compressed image to 
-     \param height pointer to the user's buffer to write the height of the compressed image to 
-     \param color_comps pointer to the user's buffer to write the number of color components of the compressed image to 
+     \param width pointer to the user's buffer to write the width of the compressed image to
+     \param height pointer to the user's buffer to write the height of the compressed image to
+     \param color_comps pointer to the user's buffer to write the number of color components of the compressed image to
     */
     virtual Status decode_info(unsigned char* input_buffer,
                                 size_t input_size,
-                                int* width, 
-                                int* height, 
+                                int* width,
+                                int* height,
                                 int* color_comps) = 0;
-    
+
     // TODO: Extend the decode API if needed, color format and order can be passed to the function
     //! Decodes the actual image data
-    /*! 
+    /*!
       \param input_buffer  User provided buffer containig the encoded image
       \param output_buffer User provided buffer used to write the decoded image into
       \param input_size Size of the compressed data provided in the input_buffer

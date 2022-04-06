cb08a24990fe37bdf08c367097d32fccbcf20777
Kiriti Gowda <kiritigowda@gmail.com>
2022-04-06 16:48:04 -0700

    ROCm CMake - Version Update to 5.1.0 (#805)
    * ROCm_CMake - Version Update to 5.1.0
    
    * Setup - Remove blank line

--- MIVisionX-setup.py
+++ MIVisionX-setup.py
@@ -53,6 +53,8 @@ parser.add_argument('--ffmpeg',    	type=str, default='no',
                     help='FFMPEG V4.0.4 Installation - optional (default:no) [options:yes/no]')
 parser.add_argument('--neural_net',	type=str, default='yes',
                     help='MIVisionX Neural Net Dependency Install - optional (default:yes) [options:yes/no]')
+parser.add_argument('--rocm_cmake',	type=str, default='rocm-5.1.0',
+                    help='ROCm CMAKE Version - optional (default:rocm-5.1.0) - ROCm CMAKE Version minimum required 0.7.3')
 parser.add_argument('--rocal',	 	type=str, default='yes',
                     help='MIVisionX rocAL Dependency Install - optional (default:yes) [options:yes/no]')
 parser.add_argument('--reinstall', 	type=str, default='no',
@@ -70,6 +72,7 @@ MIOpenGEMMVersion = args.miopengemm
 ProtoBufVersion = args.protobuf
 rppVersion = args.rpp
 ffmpegInstall = args.ffmpeg
+rocmCmakeVersion = args.rocm_cmake
 neuralNetInstall = args.neural_net
 raliInstall = args.rocal
 reinstall = args.reinstall
@@ -240,7 +243,7 @@ else:
     os.system('(cd '+deps_dir+'; unzip '+opencvVersion+'.zip )')
     if neuralNetInstall == 'yes':
         os.system(
-            '(cd '+deps_dir+'; git clone -b rocm-4.2.0 https://github.com/RadeonOpenCompute/rocm-cmake.git )')
+            '(cd '+deps_dir+'; git clone -b '+rocmCmakeVersion+' https://github.com/RadeonOpenCompute/rocm-cmake.git )')
         os.system(
             '(cd '+deps_dir+'; wget https://github.com/ROCmSoftwarePlatform/MIOpenGEMM/archive/'+MIOpenGEMMVersion+'.zip )')
         os.system('(cd '+deps_dir+'; unzip '+MIOpenGEMMVersion+'.zip )')

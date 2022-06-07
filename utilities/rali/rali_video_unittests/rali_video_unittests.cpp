634196b3fa9e03a49703401bc87119f16a032699
Kiriti Gowda <kiritigowda@gmail.com>
2022-06-07 11:14:53 -0700

    rocAL Unit Test - Fix & Updates (#854)
    * rocAL - Unit Test Fix
    
    * rocAL - Fix Support for OpenCV

--- utilities/rali/rali_video_unittests/rali_video_unittests.cpp
+++ utilities/rali/rali_video_unittests/rali_video_unittests.cpp
@@ -29,11 +29,20 @@ THE SOFTWARE.
 #include <string>
 #include <string.h>
 #include <sys/stat.h>
-#include <opencv2/opencv.hpp>
-#include <opencv/highgui.h>
+
 #include "rali_api.h"
 
+#include "opencv2/opencv.hpp"
 using namespace cv;
+#if USE_OPENCV_4
+#define CV_LOAD_IMAGE_COLOR IMREAD_COLOR
+#define CV_BGR2GRAY COLOR_BGR2GRAY
+#define CV_GRAY2RGB COLOR_GRAY2RGB
+#define CV_RGB2BGR COLOR_RGB2BGR
+#define CV_FONT_HERSHEY_SIMPLEX FONT_HERSHEY_SIMPLEX
+#define CV_FILLED FILLED
+#endif
+
 using namespace std::chrono;
 
 bool IsPathExist(const char *s)

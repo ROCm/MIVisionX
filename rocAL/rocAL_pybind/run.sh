108b9d51aaea4b106a96cea9c365ab836d30ffff
shobana-mcw <shobana@multicorewareinc.com>
2022-08-17 08:56:43 -0400

    [rocAL] Change rocAL pybind installation from setup.py to wheel. setup.py install is deprecated in python 3.9

--- rocAL/rocAL_pybind/run.sh
+++ rocAL/rocAL_pybind/run.sh
@@ -5,15 +5,14 @@ DEFAULT_PYTHON=$(which python$PYTHON_VERSION) ## Gets the default python
 CONDA="conda"
 if [ -n "$CONDA_DEFAULT_ENV" ]  || [ -n "$VIRTUAL_ENV" ] || [[ "$DEFAULT_PYTHON" == *"$CONDA"* ]]; then ## Checks if it is in any env then removes packages accordingly
     PYTHON_LIB_PATH=${DEFAULT_PYTHON/bin/lib}
-    EGG_FILE_PATH_ROCAL="/site-packages/amd_rocal-1.1.0-py$PYTHON_VERSION-linux-x86_64.egg"
-    ROCAL_PYTHON_LIB_PATH=$PYTHON_LIB_PATH$EGG_FILE_PATH_ROCAL
-    EGG_FILE_PATH_RALI="/site-packages/amd_rali-1.1.0-py$PYTHON_VERSION-linux-x86_64.egg" ## To be removed later
-    RALI_PYTHON_LIB_PATH=$PYTHON_LIB_PATH$EGG_FILE_PATH_RALI ## To be removed later
-    sudo rm -r "$ROCAL_PYTHON_LIB_PATH"
-    sudo rm -r "$RALI_PYTHON_LIB_PATH" ##To be removed later
+    # INSTALL_FILE_PATH_ROCAL="/site-packages/amd_rocal-1.1.0-py$PYTHON_VERSION-linux-x86_64.egg"
+    # ROCAL_PYTHON_LIB_PATH=$PYTHON_LIB_PATH$EGG_FILE_PATH_ROCAL
+    # EGG_FILE_PATH_RALI="/site-packages/amd_rali-1.1.0-py$PYTHON_VERSION-linux-x86_64.egg" ## To be removed later
+    # RALI_PYTHON_LIB_PATH=$PYTHON_LIB_PATH$EGG_FILE_PATH_RALI ## To be removed later
+    # sudo rm -r "$ROCAL_PYTHON_LIB_PATH"
+    # sudo rm -r "$RALI_PYTHON_LIB_PATH" ##To be removed later
 else
-  sudo rm -r "/usr/local/lib/python$PYTHON_VERSION/dist-packages/amd_rocal-1.1.0-py$PYTHON_VERSION-linux-x86_64.egg"
-  sudo rm -r "/usr/local/lib/python$PYTHON_VERSION/dist-packages/amd_rali-1.1.0-py$PYTHON_VERSION-linux-x86_64.egg" ##To be removed later
+    sudo rm -r "/usr/local/lib/python$PYTHON_VERSION/dist-packages/amd_rocal-1.1.0.dist-info/"
 fi
 sudo rm -r ./amd_rocal.egg-info/
 sudo rm -r ./build
@@ -27,8 +26,8 @@ if [[ $# -eq 1 ]]; then
   echo "${blue}Running setup.py with $1 ${reset}"
 
   if [[ "$1" == "--backend_ocl" ]] || [[ "$1" == "--backend_hip" ]]; then
-    sudo "$DEFAULT_PYTHON" setup.py build $1
-    sudo "$DEFAULT_PYTHON" setup.py install $1
+    sudo "$DEFAULT_PYTHON" setup.py bdist_wheel $1
+    pip$PYTHON_VERSION install ./dist/amd_rocal-1.1.0-cp39-cp39-linux_x86_64.whl
   else
     echo
     echo "The run.sh bash script runs the setup.py with OCL / HIP backends"
@@ -41,6 +40,6 @@ if [[ $# -eq 1 ]]; then
 else
   # Default Backend: --backend_hip
   echo "${blue}Running setup.py with --backend_hip ${reset}"
-  sudo "$DEFAULT_PYTHON" setup.py build --backend_hip
-  sudo "$DEFAULT_PYTHON" setup.py install --backend_hip
+  sudo "$DEFAULT_PYTHON" setup.py bdist_wheel --backend_hip
+  pip$PYTHON_VERSION install ./dist/amd_rocal-1.1.0-cp39-cp39-linux_x86_64.whl
 fi

75c04f0148d77ad37b56a59e89da286e0a289789
shobana-mcw <shobana@multicorewareinc.com>
2022-08-17 13:03:30 -0400

    [rocAL] Remove commented statement.

--- rocAL/rocAL_pybind/example/new_api/tf_petsTrainingExample/download_and_preprocess_dataset.sh
+++ rocAL/rocAL_pybind/example/new_api/tf_petsTrainingExample/download_and_preprocess_dataset.sh
@@ -13,7 +13,6 @@ printf "\nExtracting..."
 tar xzvf images.tar.gz
 tar xzvf annotations.tar.gz
 mkdir tf_pets_records
-# git clone https://github.com/LakshmiKumar23/rocALmodels.git
 git clone https://github.com/swetha097/rocALmodels.git
 git checkout TF_V2
 python3.9 rocALmodels/models/research/object_detection/dataset_tools/create_pet_tf_record.py --data_dir=./ --output_dir=tf_pets_records/ --label_map_path=rocALmodels/models/research/object_detection/data/pet_label_map.pbtxt

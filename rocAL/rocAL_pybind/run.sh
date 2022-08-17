62420445afd8329a2fd0a6f77372eeebee05681c
shobana-mcw <shobana@multicorewareinc.com>
2022-08-17 11:06:45 -0400

    [rocAL] Add fix to pick wheel from dist installation folder. Remove the old installation files in conda environment.

--- rocAL/rocAL_pybind/run.sh
+++ rocAL/rocAL_pybind/run.sh
@@ -5,12 +5,9 @@ DEFAULT_PYTHON=$(which python$PYTHON_VERSION) ## Gets the default python
 CONDA="conda"
 if [ -n "$CONDA_DEFAULT_ENV" ]  || [ -n "$VIRTUAL_ENV" ] || [[ "$DEFAULT_PYTHON" == *"$CONDA"* ]]; then ## Checks if it is in any env then removes packages accordingly
     PYTHON_LIB_PATH=${DEFAULT_PYTHON/bin/lib}
-    # INSTALL_FILE_PATH_ROCAL="/site-packages/amd_rocal-1.1.0-py$PYTHON_VERSION-linux-x86_64.egg"
-    # ROCAL_PYTHON_LIB_PATH=$PYTHON_LIB_PATH$EGG_FILE_PATH_ROCAL
-    # EGG_FILE_PATH_RALI="/site-packages/amd_rali-1.1.0-py$PYTHON_VERSION-linux-x86_64.egg" ## To be removed later
-    # RALI_PYTHON_LIB_PATH=$PYTHON_LIB_PATH$EGG_FILE_PATH_RALI ## To be removed later
-    # sudo rm -r "$ROCAL_PYTHON_LIB_PATH"
-    # sudo rm -r "$RALI_PYTHON_LIB_PATH" ##To be removed later
+    INSTALL_FILE_PATH_ROCAL="/site-packages/amd_rocal-1.1.0.dist-info/"
+    ROCAL_PYTHON_LIB_PATH=$PYTHON_LIB_PATH$INSTALL_FILE_PATH_ROCAL
+    sudo rm -r "$ROCAL_PYTHON_LIB_PATH"
 else
     sudo rm -r "/usr/local/lib/python$PYTHON_VERSION/dist-packages/amd_rocal-1.1.0.dist-info/"
 fi
@@ -27,7 +24,13 @@ if [[ $# -eq 1 ]]; then
 
   if [[ "$1" == "--backend_ocl" ]] || [[ "$1" == "--backend_hip" ]]; then
     sudo "$DEFAULT_PYTHON" setup.py bdist_wheel $1
-    pip$PYTHON_VERSION install ./dist/amd_rocal-1.1.0-cp39-cp39-linux_x86_64.whl
+    WHEEL_DIR="./dist"
+    WHEEL_NAME=""
+    for WHEEL_NAME in "$WHEEL_DIR"/*
+    do
+        echo "Going to install $WHEEL_NAME"
+    done
+    pip$PYTHON_VERSION install $WHEEL_NAME
   else
     echo
     echo "The run.sh bash script runs the setup.py with OCL / HIP backends"
@@ -41,5 +44,11 @@ else
   # Default Backend: --backend_hip
   echo "${blue}Running setup.py with --backend_hip ${reset}"
   sudo "$DEFAULT_PYTHON" setup.py bdist_wheel --backend_hip
-  pip$PYTHON_VERSION install ./dist/amd_rocal-1.1.0-cp39-cp39-linux_x86_64.whl
+  WHEEL_DIR="./dist"
+  WHEEL_NAME=""
+  for WHEEL_NAME in "$WHEEL_DIR"/*
+  do
+    echo "Going to install $WHEEL_NAME"
+  done
+  pip$PYTHON_VERSION install $WHEEL_NAME
 fi

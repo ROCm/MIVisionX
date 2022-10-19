59d3f890b75d1b385cb48b4173fef7729136ce7d
Swetha B S <swetha@multiocrewareinc.com>
2022-10-19 09:52:01 +0000

    Uninstall the pkg, then install making sure changes made to the wheel pkg is reflected

--- rocAL/rocAL_pybind/run.sh
+++ rocAL/rocAL_pybind/run.sh
@@ -2,15 +2,7 @@
 
 PYTHON_VERSION=`python3 -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";` ##Gets the Python3 version
 DEFAULT_PYTHON=$(which python$PYTHON_VERSION) ## Gets the default python
-CONDA="conda"
-if [ -n "$CONDA_DEFAULT_ENV" ]  || [ -n "$VIRTUAL_ENV" ] || [[ "$DEFAULT_PYTHON" == *"$CONDA"* ]]; then ## Checks if it is in any env then removes packages accordingly
-    PYTHON_LIB_PATH=`python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
-    INSTALL_FILE_PATH_ROCAL="/site-packages/amd_rocal-1.1.0.dist-info/"
-    ROCAL_PYTHON_LIB_PATH=$PYTHON_LIB_PATH$INSTALL_FILE_PATH_ROCAL
-    sudo rm -r "$ROCAL_PYTHON_LIB_PATH"
-else
-    sudo rm -r "/usr/local/lib/python$PYTHON_VERSION/dist-packages/amd_rocal-1.1.0.dist-info/"
-fi
+
 sudo rm -r ./amd_rocal.egg-info/
 sudo rm -r ./build
 sudo rm -r ./dist
@@ -30,7 +22,8 @@ if [[ $# -eq 1 ]]; then
     do
         echo "Going to install $WHEEL_NAME"
     done
-    pip$PYTHON_VERSION install $WHEEL_NAME
+    python$PYTHON_VERSION -m pip uninstall $WHEEL_NAME
+    python$PYTHON_VERSION -m pip install $WHEEL_NAME
   else
     echo
     echo "The run.sh bash script runs the setup.py with OCL / HIP backends"
@@ -50,5 +43,6 @@ else
   do
     echo "Going to install $WHEEL_NAME"
   done
-  pip$PYTHON_VERSION install $WHEEL_NAME
+  python$PYTHON_VERSION -m pip uninstall $WHEEL_NAME
+  python$PYTHON_VERSION -m pip install $WHEEL_NAME
 fi

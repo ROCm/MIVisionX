ef9f160380a0dfc8d11a62b228f8a959ff4848fd
rrawther <Rajy.MeeyakhanRawther@amd.com>
2022-11-01 14:59:55 -0700

    add option for decoder.py to run on gpu/cpu

--- rocAL/docs/examples/image_processing/decoder.py
+++ rocAL/docs/examples/image_processing/decoder.py
@@ -1,4 +1,5 @@
 
+import sys
 from tkinter import W
 from amd.rocal.pipeline import pipeline_def
 from amd.rocal.plugin.pytorch import ROCALClassificationIterator
@@ -8,7 +9,7 @@ import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 
 seed = 1549361629
-image_dir = "/home/rajy/work/MIVisionX-data/rocal_data/images_jpg/labels_folder"
+image_dir = "../../../../data/images/AMD-tinyDataSet/"
 batch_size = 4
 gpu_id = 0
 
@@ -35,12 +36,26 @@ def show_pipeline_output(pipe, device):
     images = next(iter(data_loader))
     show_images(images[0], device)
 
-rocal_device = "cpu"
 @pipeline_def(seed=seed)
-def image_decoder_pipeline(device="cpu"):
-    jpegs, labels = fn.readers.file(file_root=image_dir, shard_id=0, num_shards=1, random_shuffle=False)
-    images = fn.decoders.image(jpegs, file_root=image_dir, device=rocal_device, output_type=types.RGB, shard_id=0, num_shards=1, random_shuffle=False)
-    return fn.resize(images, device=rocal_device, resize_x=300, resize_y=300)
+def image_decoder_pipeline(device="cpu", path=image_dir):
+    jpegs, labels = fn.readers.file(file_root=path, shard_id=0, num_shards=1, random_shuffle=False)
+    images = fn.decoders.image(jpegs, file_root=path, device=device, output_type=types.RGB, shard_id=0, num_shards=1, random_shuffle=False)
+    return fn.resize(images, device=device, resize_x=300, resize_y=300)
 
-pipe = image_decoder_pipeline(batch_size=batch_size, num_threads=1, device_id=gpu_id, rocal_cpu=True, tensor_layout=types.NHWC, reverse_channels=True, multiplier = [0.00392,0.00392,0.00392])
-show_pipeline_output(pipe, device=rocal_device)
+def main():
+    print ('Optional arguments: <cpu/gpu image_folder>')
+    bs = batch_size
+    rocal_device = "cpu"
+    img_folder = image_dir
+    if  len(sys.argv) > 1:
+      if(sys.argv[1] == "gpu"):
+          rocal_device = "gpu"
+    if  len(sys.argv) > 2:
+      img_folder = sys.argv[2]
+
+    pipe = image_decoder_pipeline(batch_size=bs, num_threads=1, device_id=gpu_id, rocal_cpu=True, tensor_layout=types.NHWC,
+                                  reverse_channels=True, multiplier = [0.00392,0.00392,0.00392], device=rocal_device, path=img_folder)
+    show_pipeline_output(pipe, device=rocal_device)
+
+if __name__ == '__main__':
+    main()

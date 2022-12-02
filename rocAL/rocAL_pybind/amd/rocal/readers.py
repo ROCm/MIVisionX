ac31b958d29f4f8a7e1b70569ce8eee9040647ef
Swetha B S <swetha@mutlicorewareinc.com>
2022-12-02 19:20:33 +0000

    Remove extra line in readers.py

--- rocAL/rocAL_pybind/amd/rocal/readers.py
+++ rocAL/rocAL_pybind/amd/rocal/readers.py
@@ -22,7 +22,6 @@ import rocal_pybind as b
 from amd.rocal.pipeline import Pipeline
 import amd.rocal.types as types
 
-
 def coco(*inputs, file_root, annotations_file='', bytes_per_sample_hint=0, dump_meta_files=False,
          dump_meta_files_path='', file_list='', initial_fill=1024,  lazy_init=False, ltrb=False, masks=False,
          meta_files_path='', num_shards=1, pad_last_batch=False, prefetch_queue_depth=1, preserve=False,

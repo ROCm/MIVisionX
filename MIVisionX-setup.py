ef3f506ae5d463c65ad020280a4ddbdfdf51bb5b
Kiriti Gowda <kiritigowda@gmail.com>
2023-01-13 16:30:47 -0800

    RPP - Upgrade to V0.99 (#1018)

--- MIVisionX-setup.py
+++ MIVisionX-setup.py
@@ -30,7 +30,7 @@ else:
 __author__ = "Kiriti Nagesh Gowda"
 __copyright__ = "Copyright 2018 - 2022, AMD ROCm MIVisionX"
 __license__ = "MIT"
-__version__ = "2.3.10"
+__version__ = "2.4.0"
 __maintainer__ = "Kiriti Nagesh Gowda"
 __email__ = "mivisionx.support@amd.com"
 __status__ = "Shipping"
@@ -43,8 +43,8 @@ parser.add_argument('--opencv',    	type=str, default='4.6.0',
                     help='OpenCV Version - optional (default:4.6.0)')
 parser.add_argument('--protobuf',  	type=str, default='3.12.4',
                     help='ProtoBuf Version - optional (default:3.12.4)')
-parser.add_argument('--rpp',   		type=str, default='0.98',
-                    help='RPP Version - optional (default:0.98)')
+parser.add_argument('--rpp',   		type=str, default='0.99',
+                    help='RPP Version - optional (default:0.99)')
 parser.add_argument('--ffmpeg',    	type=str, default='no',
                     help='FFMPEG V4.4.2 Installation - optional (default:no) [options:yes/no]')
 parser.add_argument('--neural_net',	type=str, default='yes',

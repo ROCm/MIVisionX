f13e0e5e8e0252af67a4be056f20d03b7cd99f9c
swetha097 <59434434+swetha097@users.noreply.github.com>
2023-02-16 04:01:20 +0530

    rocAL - Tf pets training (#1035)
    * Updated prerequisite for tf_pets_training
    
    * Updated README.md file
    
    * Resolved PR comments
    
    * Remove --use-feature flag
    
    * Update pip3 to python -m pip
    
    * upgrade pip in dockerfile
    
    ---------
    
    Co-authored-by: root <root@ixt-sjc2-52.local.lan>
    Co-authored-by: Akilesh2 <akilesh.giribala@multicorewareinc.com>

--- rocAL/rocAL_pybind/examples/tf_petsTrainingExample/download_and_preprocess_dataset.sh
+++ rocAL/rocAL_pybind/examples/tf_petsTrainingExample/download_and_preprocess_dataset.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 cwd=$pwd
 DATASET_URL="http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz"
 GROUNDTRUTH_URL="http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz"
@@ -14,8 +13,10 @@ tar xzvf images.tar.gz
 tar xzvf annotations.tar.gz
 mkdir tf_pets_records
 git clone https://github.com/swetha097/rocALmodels.git
+cd rocALmodels
 git checkout TF_V2
-python3.9 rocALmodels/models/research/object_detection/dataset_tools/create_pet_tf_record.py --data_dir=./ --output_dir=tf_pets_records/ --label_map_path=rocALmodels/models/research/object_detection/data/pet_label_map.pbtxt
+cd ../
+python3 rocALmodels/models/research/object_detection/dataset_tools/create_pet_tf_record.py --data_dir=./ --output_dir=tf_pets_records/ --label_map_path=rocALmodels/models/research/object_detection/data/pet_label_map.pbtxt
 cd tf_pets_records
 mkdir train
 mv pet_faces_train.record-0000* train

3c92b0b27fcd419919fde0a635700732ef4a9bf3
fiona-gladwin <fionagladwin@multicorewareinc.com>
2023-03-23 07:30:04 +0000

    code_cleanup

--- utilities/rocAL/rocAL_unittests/testAllScripts.sh
+++ utilities/rocAL/rocAL_unittests/testAllScripts.sh
@@ -1,116 +1,113 @@
 #!/bin/bash
+if [ -d build ];then 
+    rm -rf ./build/*
+else
+    mkdir build
+fi
 cd build
-image_path=/media/unittest/MIVisionX-data/rocal_data/coco/coco_10_img/train_10images_2017/
-coco_classification_path=/media/unittest/MIVisionX-data/rocal_data/coco/coco_10_img/train_10images_2017
-coco_detection_path=/media/unittest/MIVisionX-data/rocal_data/coco/coco_10_img/train_10images_2017/
-tf_classification_path=/media/unittest/MIVisionX-data/rocal_data/tf/classification
-tf_detection_path=/media/unittest/MIVisionX-data/rocal_data/tf/detection/
-caffe_path=/media/unittest/MIVisionX-data/rocal_data/caffe/classification/
-caffe2_path=/media/unittest/MIVisionX-data/rocal_data/caffe2/classification/
-mxnet_path=/media/unittest/MIVisionX-data/rocal_data/mxnet/
-output_path=../OUTPUT_FOLDER_NEW/
- 
-check=1
+cmake ..
+make -j$nproc
+
+export ROCAL_DATA_PATH=/media/unittest/MIVisionX-data/ # path to be changed by the User
+image_path=${ROCAL_DATA_PATH}/rocal_data/coco/coco_10_img/train_10images_2017/
+coco_classification_path=${ROCAL_DATA_PATH}/rocal_data/coco/coco_10_img/train_10images_2017/
+coco_detection_path=${ROCAL_DATA_PATH}/rocal_data/coco/coco_10_img/train_10images_2017/
+tf_classification_path=${ROCAL_DATA_PATH}/rocal_data/tf/classification/
+tf_detection_path=${ROCAL_DATA_PATH}/rocal_data/tf/detection/
+caffe_path=${ROCAL_DATA_PATH}/rocal_data/caffe/classification/
+caffe2_path=${ROCAL_DATA_PATH}/rocal_data/caffe2/classification/
+mxnet_path=${ROCAL_DATA_PATH}/rocal_data/mxnet/
+output_path=../OUTPUT_FOLDER_NEW_$(date +%Y-%m-%d_%H-%M-%S)/
 display=0
-cpu=0
+device=0
 width=640 
 height=480
-device="host"
-if [ $cpu -eq $check ]
+device_name="host"
+
+mkdir $output_path
+if [ $device -eq 1 ]
 then 
-    device="hip"
+    device_name="hip"
+    echo "Running HIP Backend"
+else
+    echo "Running on HOST Backend"
 fi
+
  #FileSource Reader
-./rocAL_unittests 0 $image_path ${output_path}LensCorrection_rgb_${device}  $width $height 45 $cpu 1 0 $display
-./rocAL_unittests 0 $image_path ${output_path}Exposure_rgb_${device}  $width $height 46 $cpu 1 0 $display
-./rocAL_unittests 0 $image_path ${output_path}Flip_rgb_${device}  $width $height 47 $cpu 1 0 $display
+./rocAL_unittests 0 $image_path ${output_path}LensCorrection_rgb_${device_name} $width $height 45 $device 1 0 $display
+./rocAL_unittests 0 $image_path ${output_path}Exposure_rgb_${device_name} $width $height 46 $device 1 0 $display
+./rocAL_unittests 0 $image_path ${output_path}Flip_rgb_${device_name} $width $height 47 $device 1 0 $display
 
 #coco detection
-./rocAL_unittests 2 $coco_detection_path ${output_path}Gamma_rgb_${device}  $width $height 33 $cpu 1 0 $display
-./rocAL_unittests 2 $coco_detection_path ${output_path}Contrast_rgb_${device}  $width $height 34 $cpu 1 0 $display
-./rocAL_unittests 2 $coco_detection_path ${output_path}Vignette_rgb_${device}  $width $height 38 0 1 $cpu $display
+./rocAL_unittests 2 $coco_detection_path ${output_path}Gamma_rgb_${device_name} $width $height 33 $device 1 0 $display
+./rocAL_unittests 2 $coco_detection_path ${output_path}Contrast_rgb_${device_name} $width $height 34 $device 1 0 $display
+./rocAL_unittests 2 $coco_detection_path ${output_path}Vignette_rgb_${device_name} $width $height 38 0 1 $device $display
 
 #tf classification
-./rocAL_unittests 4 $tf_classification_path ${output_path}Blend_rgb_${device} $width $height 36 $cpu 1 0 $display
-./rocAL_unittests 4 $tf_classification_path ${output_path}WarpAffine_rgb_${device}  $width $height 37 $cpu 1 0 $display
-./rocAL_unittests 4 $tf_classification_path ${output_path}Blur_rgb_${device}  $width $height 35 $cpu 1 0 $display
+./rocAL_unittests 4 $tf_classification_path ${output_path}Blend_rgb_${device_name} $width $height 36 $device 1 0 $display
+./rocAL_unittests 4 $tf_classification_path ${output_path}WarpAffine_rgb_${device_name} $width $height 37 $device 1 0 $display
+./rocAL_unittests 4 $tf_classification_path ${output_path}Blur_rgb_${device_name} $width $height 35 $device 1 0 $display
 
 #tf detection
-./rocAL_unittests 5 $tf_detection_path ${output_path}CropMirrorNormalize_rgb_${device}_tfDetection  $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 5 $tf_detection_path ${output_path}ColorTemp_rgb_${device}  $width $height 43 $cpu 1 0 $display
-./rocAL_unittests 5 $tf_detection_path ${output_path}Crop_rgb_${device}_tfDetection  $width $height 51 $cpu 1 0 $display
-
+./rocAL_unittests 5 $tf_detection_path ${output_path}SNPNoise_rgb_${device_name} $width $height 40 $device 1 0 $display
+./rocAL_unittests 5 $tf_detection_path ${output_path}ColorTemp_rgb_${device_name} $width $height 43 $device 1 0 $display
+./rocAL_unittests 5 $tf_detection_path ${output_path}Crop_rgb_${device_name}_tfDetection $width $height 51 $device 1 0 $display
 
 # caffe classification
-./rocAL_unittests 6 $caffe_path ${output_path}Rotate_rgb_${device}  $width $height 31 $cpu 1 0 $display
-./rocAL_unittests 6 $caffe_path ${output_path}Brightness_rgb_${device} $width $height 32 $cpu 1 0 $display
-./rocAL_unittests 6 $caffe_path ${output_path}Hue_rgb_${device}  $width $height 48 $cpu 1 0 $display
+./rocAL_unittests 6 $caffe_path ${output_path}Rotate_rgb_${device_name} $width $height 31 $device 1 0 $display
+./rocAL_unittests 6 $caffe_path ${output_path}Brightness_rgb_${device_name} $width $height 32 $device 1 0 $display
+./rocAL_unittests 6 $caffe_path ${output_path}Hue_rgb_${device_name} $width $height 48 $device 1 0 $display
 
 # caffe detection
-./rocAL_unittests 7 $caffe_path ${output_path}Saturation_rgb_${device}  $width $height 49 $cpu 1 0 $display
-./rocAL_unittests 7 $caffe_path ${output_path}ColorTwist_rgb_${device}  $width $height 50 $cpu 1 0 $display
-./rocAL_unittests 7 $caffe_path ${output_path}Crop_rgb_${device}_caffeDetection  $width $height 51 $cpu 1 0 $display
+./rocAL_unittests 7 $caffe_path ${output_path}Saturation_rgb_${device_name} $width $height 49 $device 1 0 $display
+./rocAL_unittests 7 $caffe_path ${output_path}ColorTwist_rgb_${device_name} $width $height 50 $device 1 0 $display
+./rocAL_unittests 7 $caffe_path ${output_path}Rain_rgb_${device_name} $width $height 42 $device 1 0 $display
 
 # caffe2 classification
-./rocAL_unittests 8 $caffe2_path ${output_path}CropCenter_rgb_${device}  $width $height 52 $cpu 1 0 $display
-./rocAL_unittests 8 $caffe2_path ${output_path}ResizeCropMirror_rgb_${device}  $width $height 53 $cpu 1 0 $display
-./rocAL_unittests 8 $caffe2_path ${output_path}Resize_rgb_${device}_lanczos_default_caffe2Classification  $width $height 0 $cpu 1 0 $display 3 0
+./rocAL_unittests 8 $caffe2_path ${output_path}CropCenter_rgb_${device_name} $width $height 52 $device 1 0 $display
+./rocAL_unittests 8 $caffe2_path ${output_path}ResizeCropMirror_rgb_${device_name} $width $height 53 $device 1 0 $display
+./rocAL_unittests 8 $caffe2_path ${output_path}Snow_rgb_${device_name} $width $height 41 $device 1 0 $display
 
 # caffe2 detection
-./rocAL_unittests 9 $caffe2_path ${output_path}FishEye_rgb_${device}  $width $height 10 $cpu 1 0 $display
-./rocAL_unittests 9 $caffe2_path ${output_path}Pixelate_rgb_${device}  $width $height 19 $cpu 1 0 $display
-./rocAL_unittests 9 $caffe2_path ${output_path}CropMirrorNormalize_rgb_${device}  $width $height 55 $cpu 1 0 $display
+./rocAL_unittests 9 $caffe2_path ${output_path}FishEye_rgb_${device_name} $width $height 10 $device 1 0 $display
+./rocAL_unittests 9 $caffe2_path ${output_path}Pixelate_rgb_${device_name} $width $height 19 $device 1 0 $display
+./rocAL_unittests 9 $caffe2_path ${output_path}CropMirrorNormalize_rgb_${device_name}_caffe2Detection $width $height 55 $device 1 0 $display
 
 #mxnet 
-./rocAL_unittests 11 $mxnet_path ${output_path}Resize_rgb_${device}_gaussian_default_mxnet  $width $height 0 $cpu 1 0 $display 5 0 5 0 
-./rocAL_unittests 11 $mxnet_path ${output_path}Pixelate_rgb_${device}  $width $height 19 $cpu 1 0 $display
-./rocAL_unittests 11 $mxnet_path ${output_path}CropMirrorNormalize_rgb_${device}  $width $height 55 $cpu 1 0 $display
-
+./rocAL_unittests 11 $mxnet_path ${output_path}Jitter_rgb_${device_name} $width $height 39 $device 1 0 $display
+./rocAL_unittests 11 $mxnet_path ${output_path}Pixelate_rgb_${device_name} $width $height 19 $device 1 0 $display
+./rocAL_unittests 11 $mxnet_path ${output_path}CropMirrorNormalize_rgb_${device_name}_mxnet $width $height 55 $device 1 0 $display
 
 #CMN 
-./rocAL_unittests 0 $image_path ${output_path}CropMirrorNormalize_rgb_${device}_FileReader  $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 2 $coco_detection_path ${output_path}CropMirrorNormalize_rgb_${device}_coco  $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 4 $tf_classification_path ${output_path}CropMirrorNormalize_rgb_${device}_tfClassification $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 5 $tf_detection_path ${output_path}CropMirrorNormalize_rgb_${device}_tfDetection  $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 6 $caffe_path ${output_path}CropMirrorNormalize_rgb_${device}_caffeClassification  $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 7 $caffe_path ${output_path}CropMirrorNormalize_rgb_${device}_caffeDetection  $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 8 $caffe2_path ${output_path}CropMirrorNormalize_rgb_${device}_caffe2Classification  $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 9 $caffe2_path ${output_path}CropMirrorNormalize_rgb_${device}_caffe2Detection  $width $height 55 $cpu 1 0 $display
-./rocAL_unittests 11 $mxnet_path ${output_path}CropMirrorNormalize_rgb_${device}_mxnet  $width $height 55 $cpu 1 0 $display
+./rocAL_unittests 0 $image_path ${output_path}CropMirrorNormalize_rgb_${device_name}_FileReader $width $height 55 $device 1 0 $display
+./rocAL_unittests 2 $coco_detection_path ${output_path}CropMirrorNormalize_rgb_${device_name}_coco $width $height 55 $device 1 0 $display
+./rocAL_unittests 4 $tf_classification_path ${output_path}CropMirrorNormalize_rgb_${device_name}_tfClassification $width $height 55 $device 1 0 $display
+./rocAL_unittests 5 $tf_detection_path ${output_path}CropMirrorNormalize_rgb_${device_name}_tfDetection $width $height 55 $device 1 0 $display
+./rocAL_unittests 6 $caffe_path ${output_path}CropMirrorNormalize_rgb_${device_name}_caffeClassification $width $height 55 $device 1 0 $display
+./rocAL_unittests 7 $caffe_path ${output_path}CropMirrorNormalize_rgb_${device_name}_caffeDetection $width $height 55 $device 1 0 $display
+./rocAL_unittests 8 $caffe2_path ${output_path}CropMirrorNormalize_rgb_${device_name}_caffe2Classification $width $height 55 $device 1 0 $display
+./rocAL_unittests 9 $caffe2_path ${output_path}CropMirrorNormalize_rgb_${device_name}_caffe2Detection $width $height 55 $device 1 0 $display
+./rocAL_unittests 11 $mxnet_path ${output_path}CropMirrorNormalize_rgb_${device_name}_mxnet $width $height 55 $device 1 0 $display
 
 # crop
-./rocAL_unittests 0 $image_path ${output_path}Crop_rgb_${device}_FileReader  $width $height 51 $cpu 1 0 $display
-./rocAL_unittests 2 $coco_detection_path ${output_path}Crop_rgb_${device}_coco  $width $height 51 $cpu 1 0 $display
-./rocAL_unittests 4 $tf_classification_path ${output_path}Crop_rgb_${device}_tfClassification $width $height 51 $cpu 1 0 $display
-./rocAL_unittests 5 $tf_detection_path ${output_path}Crop_rgb_${device}_tfDetection  $width $height 51 $cpu 1 0 $display
-./rocAL_unittests 6 $caffe_path ${output_path}Crop_rgb_${device}_caffeClassification  $width $height 51 $cpu 1 0 $display
-./rocAL_unittests 7 $caffe_path ${output_path}Crop_rgb_${device}_caffeDetection  $width $height 51 $cpu 1 0 $display
-./rocAL_unittests 8 $caffe2_path ${output_path}Crop_rgb_${device}_caffe2Classification  $width $height 51 $cpu 1 0 $display
-./rocAL_unittests 9 $caffe2_path ${output_path}Crop_rgb_${device}_caffe2Detection  $width $height 51 $cpu 1 0 $display
-./rocAL_unittests 11 $mxnet_path ${output_path}Crop_rgb_${device}_mxnet  $width $height 51 $cpu 1 0 $display
-
+./rocAL_unittests 0 $image_path ${output_path}Crop_rgb_${device_name}_FileReader $width $height 51 $device 1 0 $display
+./rocAL_unittests 2 $coco_detection_path ${output_path}Crop_rgb_${device_name}_coco $width $height 51 $device 1 0 $display
+./rocAL_unittests 4 $tf_classification_path ${output_path}Crop_rgb_${device_name}_tfClassification $width $height 51 $device 1 0 $display
+./rocAL_unittests 5 $tf_detection_path ${output_path}Crop_rgb_${device_name}_tfDetection $width $height 51 $device 1 0 $display
+./rocAL_unittests 6 $caffe_path ${output_path}Crop_rgb_${device_name}_caffeClassification $width $height 51 $device 1 0 $display
+./rocAL_unittests 7 $caffe_path ${output_path}Crop_rgb_${device_name}_caffeDetection $width $height 51 $device 1 0 $display
+./rocAL_unittests 8 $caffe2_path ${output_path}Crop_rgb_${device_name}_caffe2Classification $width $height 51 $device 1 0 $display
+./rocAL_unittests 9 $caffe2_path ${output_path}Crop_rgb_${device_name}_caffe2Detection $width $height 51 $device 1 0 $display
+./rocAL_unittests 11 $mxnet_path ${output_path}Crop_rgb_${device_name}_mxnet $width $height 51 $device 1 0 $display
 
 #resize
 # Last two parameter are interpolation type and scaling mode
-
-./rocAL_unittests 0 $image_path ${output_path}Resize_rgb_${device}_bilinear_default_FileReader  $width $height 0 $cpu 1 0 $display 1 0 
-./rocAL_unittests 2 $coco_detection_path ${output_path}Resize_rgb_${device}_bilinear_stretch_coco  $width $height 0 $cpu 1 0 $display 1 1
-./rocAL_unittests 4 $tf_classification_path ${output_path}Resize_rgb_${device}_bilinear_notsmaller_tfClassification $width $height 0 $cpu 1 0 $display 1 2
-./rocAL_unittests 5 $tf_detection_path ${output_path}Resize_rgb_${device}_bilinear_notlarger_tfDetection  $width $height 0 $cpu 1 0 $display 1 3
-./rocAL_unittests 6 $caffe_path ${output_path}Resize_rgb_${device}_bicubic_default_caffeClassification  $width $height 0 $cpu 1 0 $display 1 0
-./rocAL_unittests 7 $caffe_path ${output_path}Resize_rgb_${device}_nearestneighbour_default_caffeDetection  $width $height 0 $cpu 1 0 $display 0 0 
-./rocAL_unittests 8 $caffe2_path ${output_path}Resize_rgb_${device}_lanczos_default_caffe2Classification  $width $height 0 $cpu 1 0 $display 3 0
-./rocAL_unittests 9 $caffe2_path ${output_path}Resize_rgb_${device}_triangular_default_caffe2Detection  $width $height 0 $cpu 1 0 $display 4 0
-./rocAL_unittests 11 $mxnet_path ${output_path}Resize_rgb_${device}_gaussian_default_mxnet  $width $height 0 $cpu 1 0 $display 5 0 5 0 
-
-
-
-
-
-
-
-
-
-
-
-
+./rocAL_unittests 0 $image_path ${output_path}Resize_rgb_${device_name}_bilinear_default_FileReader $width $height 0 $device 1 0 $display 1 0 
+./rocAL_unittests 2 $coco_detection_path ${output_path}Resize_rgb_${device_name}_bilinear_stretch_coco $width $height 0 $device 1 0 $display 1 1
+./rocAL_unittests 4 $tf_classification_path ${output_path}Resize_rgb_${device_name}_bilinear_notsmaller_tfClassification $width $height 0 $device 1 0 $display 1 2
+./rocAL_unittests 5 $tf_detection_path ${output_path}Resize_rgb_${device_name}_bilinear_notlarger_tfDetection $width $height 0 $device 1 0 $display 1 3
+./rocAL_unittests 6 $caffe_path ${output_path}Resize_rgb_${device_name}_bicubic_default_caffeClassification $width $height 0 $device 1 0 $display 1 0
+./rocAL_unittests 7 $caffe_path ${output_path}Resize_rgb_${device_name}_nearestneighbour_default_caffeDetection $width $height 0 $device 1 0 $display 0 0 
+./rocAL_unittests 8 $caffe2_path ${output_path}Resize_rgb_${device_name}_lanczos_default_caffe2Classification $width $height 0 $device 1 0 $display 3 0
+./rocAL_unittests 9 $caffe2_path ${output_path}Resize_rgb_${device_name}_triangular_default_caffe2Detection $width $height 0 $device 1 0 $display 4 0
+./rocAL_unittests 11 $mxnet_path ${output_path}Resize_rgb_${device_name}_gaussian_default_mxnet $width $height 0 $device 1 0 $display 5 0 

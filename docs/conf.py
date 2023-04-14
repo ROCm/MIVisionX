bb03834848c3f33b91645861dfa8ff25cb6503e3
Sam Wu <sam.wu2@amd.com>
2023-04-14 16:13:55 -0600

    update path

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@
 import os
 from rocm_docs import ROCmDocs
 
-os.system('cp -v --parents `find ../ -name "*.md"` ./docs/')
+os.system('cp -v --parents `find ../ -name "*.md"` ./')
 
 docs_core = ROCmDocs("MIVisionX Documentation")
 docs_core.setup()

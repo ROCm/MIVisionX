8a512294d15823563d292948f502645ac86326a8
hansel <hanseyang123@gmail.com>
2023-04-26 15:30:11 -0700

    using numpy_helper

--- model_compiler/python/onnx_to_nnir.py
+++ model_compiler/python/onnx_to_nnir.py
@@ -184,28 +184,31 @@ def onnx_graph_to_ir_graph(onnx_graph):
     initializerList = []
     shapeList = []
     inputUser = False
-                
+    tensorAliasList = {}
+
     for onnx_node in onnx_graph.node:
-        for tensor in onnx_graph.initializer:
-            # print(tensor)
-            # print(type(tensor.float_data))
-            # print(type(list(tensor.float_data)))
-            # quit()
-            if onnx_node.op_type == 'Reshape' and len(onnx_node.input) == 2 and tensor.name == onnx_node.input[1]:
+        if onnx_node.op_type == 'Reshape' and len(onnx_node.input) == 2:
+            for tensor in onnx_graph.initializer:
                 tensorName = onnx_name_to_ir_name(tensor.name)
-                if tensorName not in shapeList:
+                if tensor.name == onnx_node.input[1] and tensorName not in shapeList:
                     shapeList.append(tensorName)
                     graph.addVariable(onnx_tensor_info_to_data(tensor,numpy_helper.to_array(tensor)))
-                    float_array = np.array(tensor.float_data, dtype=np.float32)
-                    raw_data = numpy_helper.from_array(float_array).raw_data
+                    raw_data = numpy_helper.to_array(tensor)
                     graph.addBinary(tensorName, raw_data)
+        if onnx_node.op_type == 'Dropout':
+            tensorAliasList[onnx_node.output[0]] = onnx_node.input[0]
+        else:
+            for i in range(len(onnx_node.input)):
+                if onnx_node.input[i] in tensorAliasList:
+                    onnx_node.input[i] = tensorAliasList[onnx_node.input[i]]
+            node = onnx_node_to_ir_node(onnx_node)
+            graph.addNode(node)
     for tensor in onnx_graph.initializer:
-        if not onnx_name_to_ir_name(tensor.name) in shapeList:
-            tensorName = onnx_name_to_ir_name(tensor.name)
+        tensorName = onnx_name_to_ir_name(tensor.name)
+        if not tensorName in shapeList:
             initializerList.append(tensorName)
             graph.addVariable(onnx_tensor_info_to_data(tensor, tensor.dims))
-            float_array = np.array(tensor.float_data, dtype=np.float32)
-            raw_data = numpy_helper.from_array(float_array).raw_data
+            raw_data = numpy_helper.to_array(tensor)
             graph.addBinary(tensorName, raw_data)
     for tensor in onnx_graph.input:
         if not onnx_name_to_ir_name(tensor.name) in initializerList and not onnx_name_to_ir_name(tensor.name) in shapeList:
@@ -223,16 +226,6 @@ def onnx_graph_to_ir_graph(onnx_graph):
         while len(output_dims) != 4:
             output_dims.append(1)
         graph.addOutput(onnx_value_info_to_data(tensor, output_dims))
-    tensorAliasList = {}
-    for onnx_node in onnx_graph.node:
-        if onnx_node.op_type == 'Dropout':
-            tensorAliasList[onnx_node.output[0]] = onnx_node.input[0]
-        else:
-            for i in range(len(onnx_node.input)):
-                if onnx_node.input[i] in tensorAliasList:
-                    onnx_node.input[i] = tensorAliasList[onnx_node.input[i]]
-            node = onnx_node_to_ir_node(onnx_node)
-            graph.addNode(node)
     graph.updateLocals()
     return graph
 

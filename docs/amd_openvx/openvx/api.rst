d32115f8a7333789e082bd06088504176dc63e6d
Sam Wu <sam.wu2@amd.com>
2023-07-10 17:12:01 -0600

    define new doxygen path in conf.py

--- docs/amd_openvx/openvx/api.rst
+++ docs/amd_openvx/openvx/api.rst
@@ -2,6 +2,6 @@
 |AMD OpenVX (TM) API|
 ======================
 
-.. |AMD OpenVX (TM) API| unicode:: AMD OpenVX U+2122 API
+.. |AMD OpenVX (TM) API| unicode:: AMD U+2002 OpenVX U+2122 U+2002 API
 
 .. doxygenindex::
--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ for file_path in lines:
     os.system(f"cp {file_path[:-1]} {file_path[1:]}")
 
 docs_core = ROCmDocs("MIVisionX Documentation")
-docs_core.run_doxygen(doxygen_root=".doxygen", doxygen_path=".")
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
 docs_core.setup()
 docs_core.myst_heading_anchors = 6
 

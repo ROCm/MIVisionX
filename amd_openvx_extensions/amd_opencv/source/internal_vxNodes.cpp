e0e34aff7e6c1a8b0eff5c4abe414d23a92f0a4a
Kiriti Gowda <kiritigowda@gmail.com>
2023-07-19 16:26:39 -0700

    vx_ext_amd - updates & cleanup (#1142)
    * vx_ext_amd - updates & cleanup
    
    * Headers - Docs & Warning Fix

--- amd_openvx_extensions/amd_opencv/source/internal_vxNodes.cpp
+++ amd_openvx_extensions/amd_opencv/source/internal_vxNodes.cpp
@@ -20,8 +20,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#include "internal_publishKernels.h"
 #include "vx_ext_opencv.h"
+#include "internal_publishKernels.h"
 
 vx_node vxCreateNodeByStructure(vx_graph graph,
                                 vx_enum kernelenum,

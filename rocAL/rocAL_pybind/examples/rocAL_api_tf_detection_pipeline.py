05ce4714babc8dfb958b9c45d5a31ad5d58ff9dc
swetha097 <59434434+swetha097@users.noreply.github.com>
2023-07-19 10:39:26 +0530

    rocAL - Python Unit Test Fix PR (#1135)
    * Added Python unittest fix
    
    * Code cleanUP
    
    * Resolved Caffe and caffe2 issue
    
    * Resolved PR comments
    
    * Code cleanup
    
    * resolved PR comments
    
    * Code cleanUP
    
    * Resolved PR comments
    
    * Code cleanup
    
    * Minor code change
    
    * Minor code change
    
    * Update tf.py - default argument for init function from string to None
    
    * Update tf.py - Add None in the __init__ as default value
    
    ---------
    
    Co-authored-by: shobana-mcw <shobana@multicorewareinc.com>
    Co-authored-by: root <root@ixt-sjc2-54.local.lan>

--- rocAL/rocAL_pybind/examples/rocAL_api_tf_detection_pipeline.py
+++ rocAL/rocAL_pybind/examples/rocAL_api_tf_detection_pipeline.py
@@ -25,12 +25,13 @@ import tensorflow as tf
 import amd.rocal.fn as fn
 import numpy as np
 import os
+import cupy as cp
 from parse_config import parse_args
 
-def get_onehot(image_labels_array, numClasses):
+def get_onehot(image_labels_array, num_classes):
     one_hot_vector_list = []
     for label in image_labels_array:
-        one_hot_vector = np.zeros(numClasses)
+        one_hot_vector = np.zeros(num_classes)
         if label[0] != 0:
             np.put(one_hot_vector, label[0] - 1, 1)
         one_hot_vector_list.append(one_hot_vector)
@@ -43,13 +44,12 @@ def get_weights(num_bboxes):
     weights_array = np.zeros(100)
     for pos in list(range(num_bboxes)):
         np.put(weights_array, pos, 1)
-
     return weights_array
 
-def draw_patches(img, idx, bboxes):
-    #image is expected as a tensor, bboxes as numpy
+def draw_patches(img, idx, bboxes, device_type):
     import cv2
-    # image = img.detach().numpy()
+    if device_type == "gpu":
+        img = cp.asnumpy(img)
     image = img.transpose([0, 1, 2])
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     image = cv2.normalize(image, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
@@ -66,13 +66,14 @@ def draw_patches(img, idx, bboxes):
 def main():
     args = parse_args()
     # Args
-    imagePath = args.image_dataset_path
-    numClasses = 91
-    rocalCPU = False if args.rocal_gpu else True
+    image_path = args.image_dataset_path
+    num_classes = 91
+    rocal_cpu = False if args.rocal_gpu else True
+    device = "cpu" if rocal_cpu else "gpu"
     batch_size = args.batch_size
     num_threads = args.num_threads
-    TFRecordReaderType = 1
-    featureKeyMap = {
+    tf_record_reader_type = 1
+    feature_key_map = {
         'image/encoded': 'image/encoded',
         'image/class/label': 'image/object/class/label',
         'image/class/text': 'image/object/class/text',
@@ -84,16 +85,16 @@ def main():
     }
     try:
         path= "OUTPUT_IMAGES_PYTHON/NEW_API/TF_READER/DETECTION"
-        isExist = os.path.exists(path)
-        if not isExist:
+        is_exist = os.path.exists(path)
+        if not is_exist:
             os.makedirs(path)
     except OSError as error:
         print(error)
 
 
-    pipe = Pipeline(batch_size=batch_size, num_threads=num_threads,device_id=args.local_rank, seed=2, rocal_cpu=rocalCPU)
+    pipe = Pipeline(batch_size=batch_size, num_threads=num_threads,device_id=args.local_rank, seed=2, rocal_cpu=rocal_cpu)
     with pipe:
-        inputs = fn.readers.tfrecord(path=imagePath, index_path = "", reader_type=TFRecordReaderType, user_feature_key_map=featureKeyMap,
+        inputs = fn.readers.tfrecord(path=image_path, index_path = "", reader_type=tf_record_reader_type, user_feature_key_map=feature_key_map,
             features={
                                             'image/encoded': tf.io.FixedLenFeature((), tf.string, ""),
                                             'image/class/label': tf.io.FixedLenFeature([1], tf.int64,  -1),
@@ -107,18 +108,18 @@ def main():
         )
         jpegs = inputs["image/encoded"]
         _ = inputs["image/class/label"]
-        decoded_images = fn.decoders.image_random_crop(jpegs,user_feature_key_map=featureKeyMap, output_type=types.RGB,
+        decoded_images = fn.decoders.image_random_crop(jpegs,user_feature_key_map=feature_key_map, output_type=types.RGB,
                                                       random_aspect_ratio=[0.8, 1.25],
                                                       random_area=[0.1, 1.0],
-                                                      num_attempts=100,path = imagePath)
+                                                      num_attempts=100,path = image_path)
         resized = fn.resize(decoded_images, resize_x=300, resize_y=300)
         pipe.set_outputs(resized)
     pipe.build()
-    imageIterator = ROCALIterator(pipe)
+    image_iterator = ROCALIterator(pipe, device=device)
 
     cnt = 0
-    for i, (images_array, bboxes_array, labels_array, num_bboxes_array) in enumerate(imageIterator, 0):
-        images_array = np.transpose(images_array, [0, 2, 3, 1])
+    for i, (images_array, bboxes_array, labels_array, num_bboxes_array) in enumerate(image_iterator, 0):
+        images_array = cp.transpose(images_array, (0, 2, 3, 1)) if device == "gpu"  else np.transpose(images_array, [0, 2, 3, 1])
         print("ROCAL augmentation pipeline - Processing batch %d....." % i)
 
         for element in list(range(batch_size)):
@@ -132,16 +133,16 @@ def main():
             labels_dict = {
                 "num_groundtruth_boxes": num_bboxes_array[element],
                 "groundtruth_boxes": bboxes_array[element],
-                "groundtruth_classes": get_onehot(labels_array[element], numClasses),
+                "groundtruth_classes": get_onehot(labels_array[element], num_classes),
                 "groundtruth_weights": get_weights(num_bboxes_array[element])
             }
             processed_tensors = (features_dict, labels_dict)
             if args.print_tensor:
                 print("\nPROCESSED_TENSORS:\n", processed_tensors)
-            draw_patches(images_array[element],cnt,bboxes_array[element])
+            draw_patches(images_array[element], cnt, bboxes_array[element], device)
         print("\n\nPrinted first batch with", (batch_size), "images!")
         break
-    imageIterator.reset()
+    image_iterator.reset()
 
     print("###############################################    TF DETECTION    ###############################################")
     print("###############################################    SUCCESS         ###############################################")

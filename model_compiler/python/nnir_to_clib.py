206cca5e14d7d834657c073830814b2635a74e37
hansely <hansyang@amd.com>
2023-08-04 14:18:16 -0700

    int64 support

--- model_compiler/python/nnir_to_clib.py
+++ model_compiler/python/nnir_to_clib.py
@@ -39,6 +39,7 @@ standard_library.install_aliases()
 tensor_type_nnir2openvx = {
     'F032': 'VX_TYPE_FLOAT32',
     'F016': 'VX_TYPE_FLOAT16',
+    'U032': 'VX_TYPE_UINT32',
     'U016': 'VX_TYPE_UINT16',
     'I016': 'VX_TYPE_INT16',
     'U008': 'VX_TYPE_UINT8',
@@ -48,6 +49,7 @@ tensor_type_nnir2openvx = {
 tensor_type2size = {
     'F032': 4,
     'F016': 2,
+    'U032': 4,
     'U016': 2,
     'I016': 2,
     'U008': 1,
@@ -1246,8 +1248,8 @@ MIVID_API_ENTRY mv_status MIVID_API_CALL mvGetOutput(mivid_handle handle, int ou
         vxQueryTensor(handle->outputs[output_num], VX_TENSOR_DATA_TYPE, &data_type, sizeof(data_type));
         vxQueryTensor(handle->outputs[output_num], VX_TENSOR_NUMBER_OF_DIMS, &num_of_dims, sizeof(num_of_dims));
         vxQueryTensor(handle->outputs[output_num], VX_TENSOR_DIMS, &dims, sizeof(dims[0])*num_of_dims);
-        if((data_type != VX_TYPE_FLOAT32) && (data_type != VX_TYPE_FLOAT16)) {
-            std::cerr << "ERROR: mvGetOutput() supports only VX_TYPE_FLOAT32 or VX_TYPE_FLOAT16 " << std::endl;
+        if((data_type != VX_TYPE_FLOAT32) && (data_type != VX_TYPE_FLOAT16) && (data_type != VX_TYPE_INT64)) {
+            std::cerr << "ERROR: mvGetOutput() supports only VX_TYPE_FLOAT32 or VX_TYPE_FLOAT16 or VX_TYPE_INT64" << std::endl;
             return MV_ERROR_INVALID_TYPE;
         }
         vx_size count = dims[0] * dims[1] * dims[2] * dims[3];

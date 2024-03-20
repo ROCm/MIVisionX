e92ffe413f30fb0933004ef4e7131fe63cc6a7e9
Swetha B S <swetha@multicorewareinc.com>
2024-03-20 09:25:21 +0000

    Minor Changes

--- amd_openvx_extensions/amd_rpp/source/tensor/PreemphasisFilter.cpp
+++ amd_openvx_extensions/amd_rpp/source/tensor/PreemphasisFilter.cpp
@@ -57,7 +57,7 @@ static vx_status VX_CALLBACK refreshPreemphasisFilter(vx_node node, const vx_ref
     }
     RpptROI *src_roi = reinterpret_cast<RpptROI *>(roi_tensor_ptr_src);
     RpptROI *dst_roi = reinterpret_cast<RpptROI *>(roi_tensor_ptr_dst);
-    for (int n = data->inputTensorDims[0] - 1; n >= 0; n--)
+    for (int n = 0; n < data->inputTensorDims[0]; n++)
         data->pSampleSize[n] = src_roi[n].xywhROI.roiWidth * src_roi[n].xywhROI.roiHeight;
 
     update_destination_roi(parameters, data, src_roi, dst_roi);

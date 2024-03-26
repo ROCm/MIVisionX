b2a6d5861dc0042ef9e369460dff1f67193435f4
Swetha B S <swetha@multicorewareinc.com>
2024-03-26 16:01:03 +0000

    NWC

--- amd_openvx_extensions/amd_rpp/include/internal_publishKernels.h
+++ amd_openvx_extensions/amd_rpp/include/internal_publishKernels.h
@@ -164,6 +164,8 @@ vx_status Normalize_Register(vx_context);
 vx_status Resample_Register(vx_context);
 vx_status TensorMulScalar_Register(vx_context);
 vx_status TensorAddTensor_Register(vx_context);
+vx_status NonSilentRegion_Register(vx_context);
+vx_status Slice_Register(vx_context);
 
 // kernel names
 #define VX_KERNEL_RPP_NOPBATCHPD_NAME                           "org.rpp.NopbatchPD"
@@ -297,5 +299,7 @@ vx_status TensorAddTensor_Register(vx_context);
 #define VX_KERNEL_RPP_RESAMPLE_NAME                             "org.rpp.Resample"
 #define VX_KERNEL_RPP_TENSORMULSCALAR_NAME                      "org.rpp.TensorMulScalar"
 #define VX_KERNEL_RPP_TENSORADDTENSOR_NAME                      "org.rpp.TensorAddTensor"
+#define VX_KERNEL_RPP_NONSILENTREGION_NAME                      "org.rpp.NonSilentRegion"
+#define VX_KERNEL_RPP_SLICE_NAME                                "org.rpp.Slice"
 
 #endif //_AMDVX_EXT__PUBLISH_KERNELS_H_

4fcc2df7384050faaebc6ea52f0ac62a3a3b8fea
Kiriti Gowda <kiriti.nageshgowda@amd.com>
2024-05-16 21:37:25 -0700

    Revert "VX_RPP - Introduce Audio Layout and fix Spectrogram  (#1347)"
    This reverts commit 74e28cf67d8262bf4c1703f06447c61805e65522.

--- amd_openvx_extensions/amd_rpp/include/internal_rpp.h
+++ amd_openvx_extensions/amd_rpp/include/internal_rpp.h
@@ -68,10 +68,7 @@ enum vxTensorLayout {
     VX_NHWC = 0,
     VX_NCHW = 1,
     VX_NFHWC = 2,
-    VX_NFCHW = 3,
-    VX_NHW = 4,     // Audio/2D layout
-    VX_NFT = 5,     // Frequency major, Used for Spectrogram/MelFilterBank
-    VX_NTF = 6      // Time major, Used for Spectrogram/MelFilterBank
+    VX_NFCHW = 3
 };
 
 //! Brief The utility functions
@@ -79,9 +76,8 @@ vx_node createNode(vx_graph graph, vx_enum kernelEnum, vx_reference params[], vx
 vx_status createRPPHandle(vx_node node, vxRppHandle ** pHandle, Rpp32u batchSize, Rpp32u deviceType);
 vx_status releaseRPPHandle(vx_node node, vxRppHandle * handle, Rpp32u deviceType);
 void fillDescriptionPtrfromDims(RpptDescPtr &descPtr, vxTensorLayout layout, size_t *tensorDims);
-void fillAudioDescriptionPtrFromDims(RpptDescPtr &descPtr, size_t *tensorDims, vxTensorLayout layout = vxTensorLayout::VX_NHW);
+void fillAudioDescriptionPtrFromDims(RpptDescPtr &descPtr, size_t *tensorDims);
 RpptDataType getRpptDataType(vx_enum dataType);
-RpptLayout getRpptLayout(vxTensorLayout layout);
 
 class Kernellist
 {

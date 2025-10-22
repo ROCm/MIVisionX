71799a6b5b2fe311319f0803fb8cd314a5ee92ee
fiona-gladwin <fionagladwin@multicorewareinc.com>
2025-10-22 03:04:30 -0500

    Introduce GridMask vx_rpp extension

--- amd_openvx_extensions/amd_rpp/include/vx_ext_rpp.h
+++ amd_openvx_extensions/amd_rpp/include/vx_ext_rpp.h
@@ -1530,6 +1530,24 @@ extern "C"
 	 */
 	SHARED_PUBLIC vx_node VX_API_CALL vxExtRppColorCast(vx_graph graph, vx_tensor pSrc, vx_tensor pSrcRoi, vx_tensor pDst, vx_array pRgb, vx_array pAlpha, vx_scalar inputLayout, vx_scalar outputLayout, vx_scalar roiType);
 	
+	/*! \brief [Graph] Creates a GridMask function node.
+	 * \ingroup group_amd_rpp
+	 * \param [in] graph The handle to the graph.
+	 * \param [in] pSrc The input tensor in <tt>\ref VX_TYPE_UINT8</tt> or <tt>\ref VX_TYPE_FLOAT32</tt> or <tt>\ref VX_TYPE_FLOAT16</tt> or <tt>\ref VX_TYPE_INT8</tt> format data.
+	 * \param [in] pSrcRoi The input tensor of batch size in <tt>unsigned int</tt> containing the roi values for the input in xywh/ltrb format.
+	 * \param [out] pDst The output tensor in <tt>\ref VX_TYPE_UINT8</tt> or <tt>\ref VX_TYPE_FLOAT32</tt> or <tt>\ref VX_TYPE_FLOAT16</tt> or <tt>\ref VX_TYPE_INT8</tt> format data.
+	 * \param [in] tileWidth The input scalar in <tt>\ref VX_TYPE_UINT32</tt> denoting the grid tile width (pixels).
+	 * \param [in] gridRatio The input scalar in <tt>\ref VX_TYPE_FLOAT32</tt> denoting the ratio of masked area within each tile (0.0 to 1.0).
+	 * \param [in] gridAngle The input scalar in <tt>\ref VX_TYPE_FLOAT32</tt> denoting the grid rotation in radians.
+	 * \param [in] translateX The input scalar in <tt>\ref VX_TYPE_UINT32</tt> denoting the translation along X in pixels.
+	 * \param [in] translateY The input scalar in <tt>\ref VX_TYPE_UINT32</tt> denoting the translation along Y in pixels.
+	 * \param [in] inputLayout The input layout in <tt>\ref VX_TYPE_INT32</tt> denotes the layout of input tensor.
+	 * \param [in] outputLayout The output layout in <tt>\ref VX_TYPE_INT32</tt> denotes the layout of output tensor.
+	 * \param [in] roiType The type of roi <tt>\ref VX_TYPE_INT32</tt> denotes whether source roi is of XYWH/LTRB type.
+	 * \return A node reference <tt>\ref vx_node</tt>. Any possible errors preventing a successful creation should be checked using <tt>\ref vxGetStatus</tt>.
+	 */
+	SHARED_PUBLIC vx_node VX_API_CALL vxExtRppGridMask(vx_graph graph, vx_tensor pSrc, vx_tensor pSrcRoi, vx_tensor pDst, vx_scalar tileWidth, vx_scalar gridRatio, vx_scalar gridAngle, vx_scalar translateX, vx_scalar translateY, vx_scalar inputLayout, vx_scalar outputLayout, vx_scalar roiType);
+	
 	/*! \brief [Graph] Creates a Contrast function node.
 	 * \ingroup group_amd_rpp
 	 * \param [in] graph The handle to the graph.
--- amd_openvx_extensions/amd_rpp/source/internal_publishKernels.cpp
+++ amd_openvx_extensions/amd_rpp/source/internal_publishKernels.cpp
@@ -136,6 +136,7 @@ vx_status get_kernels_to_publish()
     STATUS_ERROR_CHECK(ADD_KERNEL(ColorTemperature_Register));
     STATUS_ERROR_CHECK(ADD_KERNEL(ColorTwist_Register));
     STATUS_ERROR_CHECK(ADD_KERNEL(ColorCast_Register));
+    STATUS_ERROR_CHECK(ADD_KERNEL(GridMask_Register));
     STATUS_ERROR_CHECK(ADD_KERNEL(Contrast_Register));
     STATUS_ERROR_CHECK(ADD_KERNEL(Copy_Register));
     STATUS_ERROR_CHECK(ADD_KERNEL(Crop_Register));

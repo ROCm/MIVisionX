a14a3a9a2c8f194b6c2de34cde8c051cdedf09d0
KiritiGowda <kiritigowda@gmail.com>
2026-06-19 10:34:56 -0700

    Vision - color convert failure fix

--- amd_openvx/openvx/ago/ago_haf_cpu_color_convert.cpp
+++ amd_openvx/openvx/ago/ago_haf_cpu_color_convert.cpp
@@ -484,6 +484,7 @@ int HafCpu_ColorConvert_RGB_IYUV
 	)
 {
 	int alignedWidth = dstWidth & ~15;
+	alignedWidth = (alignedWidth > 15) ? alignedWidth-16: 0;
 	int postfixWidth = (int)dstWidth - alignedWidth;
 
 	__m128 Y00, Y01, Y10, Y11, U, V;

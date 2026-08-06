5cb10db6a8fdbe024a0ecb11794c6e53e3ccf237
Danielle Gillai <dagillai@amd.com>
2026-08-05 20:20:02 -0700

    [Bugfix]: Compile when RPP on CPU, MIVisionX on HIP, and use correct functions when RPP on CPU (#1726)

--- amd_openvx_extensions/amd_rpp/source/tensor/TensorMean.cpp
+++ amd_openvx_extensions/amd_rpp/source/tensor/TensorMean.cpp
@@ -39,12 +39,12 @@ static vx_status VX_CALLBACK refreshTensorMean(vx_node node, const vx_reference 
     vx_status status = VX_SUCCESS;
     void *roi_tensor_ptr;
     if (data->deviceType == AGO_TARGET_AFFINITY_GPU) {
-#if ENABLE_OPENCL
-        return VX_ERROR_NOT_IMPLEMENTED;
-#elif ENABLE_HIP
+#if ENABLE_HIP
         STATUS_ERROR_CHECK(vxQueryTensor((vx_tensor)parameters[1], VX_TENSOR_BUFFER_HIP, &roi_tensor_ptr, sizeof(roi_tensor_ptr)));
         STATUS_ERROR_CHECK(vxQueryTensor((vx_tensor)parameters[0], VX_TENSOR_BUFFER_HIP, &data->pSrc, sizeof(data->pSrc)));
         STATUS_ERROR_CHECK(vxQueryTensor((vx_tensor)parameters[2], VX_TENSOR_BUFFER_HIP, &data->pDst, sizeof(data->pDst)));
+#else
+        return VX_ERROR_NOT_IMPLEMENTED;
 #endif
     } else if (data->deviceType == AGO_TARGET_AFFINITY_CPU) {
         STATUS_ERROR_CHECK(vxQueryTensor((vx_tensor)parameters[1], VX_TENSOR_BUFFER_HOST, &roi_tensor_ptr, sizeof(roi_tensor_ptr)));
